da88417f4b658300f9cd54bd36eb517f1c5537e3
Thanh Huynh <thanh@clearcanvas.ca>
2013-02-21 22:12:08 -0500

    [Defect #10890] - RT Structure Set Objects encoded wrong when saved as explicit little endian
    * Updated upgrade script
    
    --HG--
    branch : Phoenix5

--- ImageServer/Model/SqlServer/UpgradeScripts/UpgradeFrom_9_0_9561_11723.sql
+++ ImageServer/Model/SqlServer/UpgradeScripts/UpgradeFrom_9_0_9561_11723.sql
@@ -19,6 +19,16 @@ GO
 update [ServiceLockTypeEnum] set LongDescription='This service periodically synchronizes the deletion status of Authority Groups on the Enterprise Services with Data Access granted to studies on the ImageServer.' where Lookup='SyncDataAccess'
 GO
 
+PRINT N'Adding ImplicitOnly column'
+GO
+ALTER TABLE [ServerSopClass] ADD [ImplicitOnly] bit NOT NULL DEFAULT(0)
+GO
+
+PRINT N'Updating ImplicitOnly column'
+GO
+UPDATE [ServerSopClass] SET [ImplicitOnly]=1 WHERE [SopClassUid]='1.2.840.10008.5.1.4.1.1.481.3'
+GO
+
 IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
 GO
 IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
