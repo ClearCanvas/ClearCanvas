ec3ab5873e4e8c0b7c852b56c4ee87c88d09df8a
Stewart Bright <stewart@clearcanvas.ca>
2013-02-25 08:15:37 -0500

    [Bug #10901] - PNG images sent to browser contain color correction information - Added code to strip color management headers from PNGs before sending to browser. - Added unit tests for stripping code.
    --HG--
    branch : Phoenix5

--- ImageViewer/Common/Utilities/Imaging/PngEncoder.cs
+++ ImageViewer/Common/Utilities/Imaging/PngEncoder.cs
@@ -28,6 +28,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using ClearCanvas.Common;
 
@@ -44,22 +45,50 @@ namespace ClearCanvas.ImageViewer.Common.Utilities.Imaging
     {
     }
 
-    public abstract class PngEncoder : IPngEncoder
+    public abstract partial class PngEncoder : IPngEncoder
     {
-        private class DefaultEncoder : IPngEncoder
+        internal class ChunkInfo
+        {
+            public int DataLength;
+            public string Type;
+
+            public int TotalLength
+            {
+                get
+                {
+                    //Length,Type,CRC are all 4 bytes, plus the length of the actual data.
+                    return ChunkInfoLength*3 + DataLength;
+                }
+            }
+
+            public bool IsColorManagement
+            {
+                get { return IsColorManagementType(Type); }
+            }
+        }
+
+        internal class DefaultEncoder : IPngEncoder
         {
             #region IPngEncoder Members
 
             public MemoryStream Encode(Bitmap bitmap)
             {
-                var stream = new MemoryStream();
-                bitmap.Save(stream, ImageFormat.Png);
-                return stream;
+                using (var pngStream = new MemoryStream())
+                {
+                    bitmap.Save(pngStream, ImageFormat.Png);
+                    var strippedStream = new MemoryStream();
+                    StripColorManagement(pngStream, strippedStream);
+                    return strippedStream;
+                }
             }
 
             public void Encode(Bitmap bitmap, MemoryStream memoryStream)
             {
-                bitmap.Save(memoryStream, ImageFormat.Png);
+                using (var tempStream = new MemoryStream())
+                {
+                    bitmap.Save(tempStream, ImageFormat.Png);
+                    StripColorManagement(tempStream, memoryStream);
+                }
             }
 
             #endregion
@@ -72,7 +101,7 @@ namespace ClearCanvas.ImageViewer.Common.Utilities.Imaging
         {
             try
             {
-                return (IPngEncoder)new PngEncoderExtensionPoint().CreateExtension();
+                return (IPngEncoder) new PngEncoderExtensionPoint().CreateExtension();
             }
             catch (NotSupportedException)
             {
@@ -80,5 +109,159 @@ namespace ClearCanvas.ImageViewer.Common.Utilities.Imaging
 
             return new DefaultEncoder();
         }
+
     }
+
+    #region Parser
+
+    public abstract partial class PngEncoder
+    {
+        //Length of chunk length, type and CRC sections.
+        internal const int ChunkInfoLength = 4;
+
+        private static readonly string[] _colorManagementTypes = new string[] {"gAMA", "iCCP", "sRGB", "cHRM"};
+
+        //All PNGs have this exact 8 byte header.
+        private static readonly byte[] _pngHeader = new byte[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+
+        internal static void StripColorManagement(MemoryStream pngStream, MemoryStream outStream)
+        {
+            pngStream.Position = outStream.Position = 0;
+            var header = ReadHeader(pngStream);
+
+            //write the header
+            outStream.Write(header, 0, header.Length);
+
+            const int chunkInfoLength = 4;
+            const int dataBufferLength = 16384;
+            var lengthBuffer = new byte[chunkInfoLength];
+            var typeBuffer = new byte[chunkInfoLength];
+            var crcBuffer = new byte[chunkInfoLength];
+            var dataBuffer = new byte[dataBufferLength];
+
+            while (pngStream.Position < pngStream.Length)
+            {
+                //Read chunk length
+                int chunkDataLength = ReadChunkDataLength(pngStream, lengthBuffer);
+                string chunkType = ReadChunkType(pngStream, typeBuffer);
+
+                if (IsColorManagementType(chunkType))
+                {
+                    //Skip this whole chunk
+                    SkipChunkDataAndCRC(pngStream, chunkDataLength);
+                    continue;
+                }
+
+                //Write out the chunk length and type
+                outStream.Write(lengthBuffer, 0, lengthBuffer.Length);
+                outStream.Write(typeBuffer, 0, typeBuffer.Length);
+
+                //Read the chunk data in and then write it directly out.
+                var bytesLeftToRead = chunkDataLength;
+                while (bytesLeftToRead > 0)
+                {
+                    //Read the smaller of the bytes left to be read, or the buffer's actual length
+                    var bytesToRead = Math.Min(dataBuffer.Length, bytesLeftToRead);
+                    int bytesRead = pngStream.Read(dataBuffer, 0, bytesToRead);
+                    if (bytesToRead != bytesRead)
+                        throw new System.IO.InvalidDataException("PNG appears to be truncated.");
+
+                    outStream.Write(dataBuffer, 0, bytesRead);
+                    bytesLeftToRead -= bytesRead;
+                }
+
+                //Read the CRC and write it out.
+                if (chunkInfoLength != pngStream.Read(crcBuffer, 0, chunkInfoLength))
+                    throw new System.IO.InvalidDataException("PNG must have a 4 byte CRC field after data.");
+
+                outStream.Write(crcBuffer, 0, chunkInfoLength);
+            }
+
+            //Nothing got written out but the header.
+            if (outStream.Length <= _pngHeader.Length)
+                throw new System.IO.InvalidDataException("PNG stream is invalid.");
+
+            outStream.Position = 0;
+        }
+
+        internal static ChunkInfo[] GetChunkInfo(MemoryStream pngStream)
+        {
+            var chunkInfo = new List<ChunkInfo>();
+
+            pngStream.Position = 0;
+            ReadHeader(pngStream);
+
+            const int chunkInfoLength = 4;
+            var lengthBuffer = new byte[chunkInfoLength];
+            var typeBuffer = new byte[chunkInfoLength];
+            
+            while (pngStream.Position < pngStream.Length)
+            {
+                int dataLength = ReadChunkDataLength(pngStream, lengthBuffer);
+                string type = ReadChunkType(pngStream, typeBuffer);
+                chunkInfo.Add(new ChunkInfo{Type = type, DataLength = dataLength});
+                SkipChunkDataAndCRC(pngStream, dataLength);
+            }
+
+            return chunkInfo.ToArray();
+        }
+
+        internal static byte[] ReadHeader(MemoryStream pngStream)
+        {
+            //Since we're reading directly from a PNG we just saved, all these checks are kinda overkill, but it doesn't hurt in case we use it elsewhere later.
+            var header = new byte[_pngHeader.Length];
+            //read the 8-byte PNG header
+            var bytesRead = pngStream.Read(header, 0, header.Length);
+            if (bytesRead != header.Length)
+                throw new System.IO.InvalidDataException("PNG header must be 8 bytes long.");
+
+            if (!header.SequenceEqual(_pngHeader))
+                throw new System.IO.InvalidDataException("PNG has an invalid header.");
+            return header;
+        }
+
+        internal static int ReadChunkDataLength(MemoryStream pngStream, byte[] lengthBuffer)
+        {
+            if (lengthBuffer.Length != ChunkInfoLength)
+                throw new ArgumentException("Input buffer must be 4 bytes long.");
+
+            var bytesRead = pngStream.Read(lengthBuffer, 0, ChunkInfoLength);
+            if (bytesRead != ChunkInfoLength)
+                throw new System.IO.InvalidDataException("PNG chunk must have a 4 byte length field at the beginning.");
+
+            unsafe
+            {
+                fixed (byte* length = lengthBuffer)
+                {
+                    //Network order is big endian, which is how PNGs are encoded, so convert to machine format.
+                    return IPAddress.NetworkToHostOrder(*(Int32*) length);
+                }
+            }
+        }
+
+        internal static string ReadChunkType(MemoryStream pngStream, byte[] typeBuffer)
+        {
+            if (typeBuffer.Length != ChunkInfoLength)
+                throw new ArgumentException("Input buffer must be 4 bytes long.");
+
+            //Read chunk type
+            if (ChunkInfoLength != pngStream.Read(typeBuffer, 0, ChunkInfoLength))
+                throw new System.IO.InvalidDataException("PNG chunk must have a 4 byte type following the length.");
+
+            return Encoding.ASCII.GetString(typeBuffer);
+        }
+
+        internal static void SkipChunkDataAndCRC(MemoryStream pngStream, int dataLength)
+        {
+            pngStream.Position += dataLength; //skip value
+            pngStream.Position += ChunkInfoLength; //skip CRC
+        }
+
+        internal static bool IsColorManagementType(string type)
+        {
+            return _colorManagementTypes.Contains(type);
+        }
+    }
+
+    #endregion
 }
