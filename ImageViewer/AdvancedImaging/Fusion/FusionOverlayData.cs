1adf23a13fdea449defa529b36bc0d8d1597cdaf
Stewart Bright <stewart@clearcanvas.ca>
2013-04-16 15:31:22 -0400

    [Bug #11002] - Fusion crashes while being memory managed [Requirement #10383] - Perform design reviews - Added a few code review comments that do not have to be addressed now. - At some point, we should refactor Fusion to better separate concerns, as it's difficult currently to figure out what's happening synchronously vs. async, when the progress bar will show up, and also what might trigger Fusion to load synchronously on the UI thread. From code inspection, it looks like SUV and Window/Level synchronization could cause Fusion to load synchronously just like the issue we saw with MPR (#11003); however, it's less of a concern with Fusion because PET volumes are generally loaded much faster because of their relatively small size.
    --HG--
    branch : Phoenix5

--- ImageViewer/AdvancedImaging/Fusion/FusionOverlayData.cs
+++ ImageViewer/AdvancedImaging/Fusion/FusionOverlayData.cs
@@ -139,6 +139,13 @@ namespace ClearCanvas.ImageViewer.AdvancedImaging.Fusion
 
 		private VolumeData LoadVolume(IBackgroundTaskContext context)
 		{
+		    // TODO (CR Apr 2013): Ideally, loading and unloading could be done with minimal locking; this way,
+            // Unload actually has to wait for Load to finish and vice versa. I think a quicker way would be to
+            // have a _loading field - have this method set it inside the lock, then proceed to do the load,
+            // then set the _volume field when done. Have Unload check _loading and just return, otherwise set _volume to null.
+            // Basically, you don't need to lock the entire load operation - you only need to guarantee that multiple loads
+            // can't occur at once, and that Unload actually unloads it.
+
 			// wait for synchronized access
 			lock (_syncVolumeDataLock)
 			{
@@ -428,6 +435,8 @@ namespace ClearCanvas.ImageViewer.AdvancedImaging.Fusion
 				}
 				else
 				{
+                    // TODO (CR Apr 2013): See comment in OnVolumeLoaderTaskTerminated; if the task were not created
+                    // on the UI thread, _volumeLoaderTask could be set to null before hitting this instruction.
 					if (_volumeLoaderTask.LastBackgroundTaskProgress != null)
 					{
 						message = _volumeLoaderTask.LastBackgroundTaskProgress.Progress.Message;
@@ -457,13 +466,18 @@ namespace ClearCanvas.ImageViewer.AdvancedImaging.Fusion
 
 		private void OnVolumeLoaderTaskTerminated(object sender, BackgroundTaskTerminatedEventArgs e)
 		{
-			BackgroundTask volumeLoaderTask = sender as BackgroundTask;
+            // TODO (CR Apr 2013): Since BeginLoad is only ever triggered from the UI thread (Draw), this won't be
+            // a problem because the task's Terminated event will also be fired on the UI thread. If the task
+            // were not created on the UI thread, though, this would have to be inside a lock (_syncLoaderLock).
+            
+            BackgroundTask volumeLoaderTask = sender as BackgroundTask;
 			if (volumeLoaderTask != null)
 			{
 				volumeLoaderTask.Terminated -= OnVolumeLoaderTaskTerminated;
 				volumeLoaderTask.Dispose();
 			}
-			_volumeLoaderTask = null;
+
+            _volumeLoaderTask = null;
 		}
 
 		#endregion
@@ -472,6 +486,7 @@ namespace ClearCanvas.ImageViewer.AdvancedImaging.Fusion
 
 		bool IProgressGraphicProgressProvider.IsRunning(out float progress, out string message)
 		{
+		    // TODO (CR Apr 2013): Misleading that IsRunning might actually trigger a load.
 			return !BeginLoad(out progress, out message);
 		}
 
