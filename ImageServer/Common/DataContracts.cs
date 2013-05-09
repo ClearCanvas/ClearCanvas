af688b47bf24491f0e117572cc21ad5d62e0faed
Steve Wranovsky <steve@clearcanvas.ca>
2013-05-09 00:11:24 -0500

    [Ticket #11148] - The ImageServer shall support a WCF API for configuration of research partitions
    Preliminary updates to include some data contracts for the new web services.
    This still needs a bit of work.  Also created a new CC.IS.Research.Services.Common
    and CC.IS.Research.Services.Core assemblies that will eventually be used for
    research PACS specific services.
    
    In any case, the general idea is that a client would just need to include these three IS
    specific assemblies to get the data contracts and bridge classes to access
    them:
    
    ClearCanvas.ImageServer.Common
    ClearCanvas.ImageServer.Enterprise.Services.Common
    ClearCanvas.ImageServer.Research.Services.Common
    
    --HG--
    branch : Rigel

---
+++ ImageServer/Common/DataContracts.cs
@@ -0,0 +1,56 @@
+﻿#region License
+
+// Copyright (c) 2013, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This file is part of the ClearCanvas RIS/PACS open source project.
+//
+// The ClearCanvas RIS/PACS open source project is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// The ClearCanvas RIS/PACS open source project is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the ClearCanvas RIS/PACS open source project.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using System.Runtime.Serialization;
+using ClearCanvas.Common.Serialization;
+
+namespace ClearCanvas.ImageServer.Common
+{
+    public static class ImageServerNamespace
+    {
+        public const string Value = "http://www.clearcanvas.ca/imageserver";
+    }
+
+    public abstract class ImageServerRequestItem : DataContractBase
+    {
+
+
+    }
+
+    public abstract class ImageServerProgressItem : DataContractBase
+    {
+        public virtual string Status { get { return string.Empty; } }
+
+        [DataMember(IsRequired = false)]
+        public string StatusDetails { get; set; }
+
+        public virtual Decimal PercentComplete { get { return new decimal(0.0); } }
+
+        public virtual Decimal PercentFailed { get { return new decimal(0.0); } }
+
+        [DataMember(IsRequired = true)]
+        public bool IsCancelable { get; set; }
+    }
+}
