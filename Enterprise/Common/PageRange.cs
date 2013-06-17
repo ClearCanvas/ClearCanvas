9dd11f510ce0731f12da1b18b933ab4f9b8656c4
Jonathan Resnick <jonathan@clearcanvas.ca>
2013-06-17 19:39:15 -0400

    [Ticket #11142] - The research PACS shall be accessible via a REST API
    Added in some infrastructure to support pagination at the database level, which is necessary so that we can ultimately support pagination at the REST API level.
     * The existing SearchResultPage class in Enterprise.Common is designed for the "old way" of doing paging and is not suited to the "new way" of doing it (the old way is to supply the index of the first desired item, whereas the new way relies on passing back a position token obtained from the previous call). For this reason, needed to add some new classes to Enterprise.Common: these are called PageRange and PagedResults.
     * Added method PersistenceBroker.ExecutePaged(...) to help with some of the boilerplate stuff that needs to happen.
     * Updated all Research brokers to consume PageRange (instead of SearchResultPage) and return PagedResults (instead of IList) where appropriate.
     * Paging is not yet exposed through the REST API.
    
    --HG--
    branch : Rigel

---
+++ Enterprise/Common/PageRange.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ClearCanvas.Enterprise.Common
+{
+	/// <summary>
+	/// Represents a page of a result set.
+	/// </summary>
+	public struct PageRange : IEquatable<PageRange>
+	{
+		/// <summary>
+		/// Gets a page range that includes all items.
+		/// </summary>
+		public static readonly PageRange All = new PageRange(null, int.MaxValue - 1);
+
+		/// <summary>
+		/// Gets the position token that identifies the starting point.
+		/// </summary>
+		public readonly string PositionToken;
+
+		/// <summary>
+		/// Gets the number of items to return.
+		/// </summary>
+		public readonly int MaxItems;
+
+		/// <summary>
+		/// Constructs a page range for the specified number of items starting at the first result.
+		/// </summary>
+		public PageRange(int maxItems)
+			:this(null, maxItems)
+		{
+		}
+
+		/// <summary>
+		/// Constructs a page range at the specified starting position, for the specified number of items.
+		/// </summary>
+		public PageRange(string positionToken, int maxItems)
+		{
+			PositionToken = positionToken;
+			MaxItems = maxItems;
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this page range represents the first page of results.
+		/// </summary>
+		public bool IsFirst
+		{
+			get { return PositionToken == null; }
+		}
+
+		public bool Equals(PageRange other)
+		{
+			return Equals(other.PositionToken, PositionToken) && other.MaxItems == MaxItems;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (obj.GetType() != typeof (PageRange)) return false;
+			return Equals((PageRange) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((PositionToken != null ? PositionToken.GetHashCode() : 0) * 397) ^ MaxItems.GetHashCode();
+			}
+		}
+
+		public static bool operator ==(PageRange left, PageRange right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(PageRange left, PageRange right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
