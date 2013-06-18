6f90daf7f4f4233a3f9ed10eb45816709fc2207e
Stewart Bright <stewart@clearcanvas.ca>
2013-06-18 11:21:42 -0400

    [Requirement #11215] - Research PACS shall support deriving studies for anonymization of studies - Removed ImageServer.Common (Xian) dependency on Dicom.Editing.Common (Jin). Moved only the base Edit and Condition data contract classes, as well as the EditTypeAttribute to Dicom.ServiceModel.Editing. All the important contracts, as well as DicomXPath remain in Jin, at least for now. - Added RemovePrivateAttributes to serialization tests. - Added Jsml serialization test as well as data contract test, since both methods of serialization are used in production code.
    --HG--
    branch : Rigel

---
+++ Dicom/ServiceModel/Editing/Condition.cs
@@ -0,0 +1,69 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+// 
+// For information about the licensing and copyright of this software please
+// contact ClearCanvas, Inc. at info@clearcanvas.ca
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Serialization;
+
+namespace ClearCanvas.Dicom.ServiceModel.Editing
+{
+    [KnownType("GetKnownTypes")]
+    [DataContract(Namespace = DicomEditNamespace.Value)]
+    public abstract class Condition : DataContractBase
+    {
+        private static readonly object _syncLock = new object();
+        private static volatile IList<Type> _types;
+
+        static Condition()
+        {
+        }
+
+        /// <summary>
+        /// Gets all known types derived from <see cref="Condition"/>.
+        /// </summary>
+        public static IEnumerable<Type> GetKnownTypes()
+        {
+            if (_types != null)
+                return _types;
+
+            lock (_syncLock)
+            {
+                if (_types != null)
+                    return _types;
+
+                var assemblies = (from plugin in Platform.PluginManager.Plugins select plugin.Assembly.Resolve()).ToList();
+                assemblies.Add(typeof(Edit).Assembly);
+
+                return _types = (from assembly in assemblies
+                                 from type in assembly.GetTypes()
+                                 where typeof(Condition).IsAssignableFrom(type)
+                                       && type.IsDefined(typeof (DataContractAttribute), false)
+                                       && type.IsDefined(typeof (EditTypeAttribute), false)
+                                 select type).ToList().AsReadOnly();
+            }
+        }
+
+#if UNIT_TESTS
+        /// <summary>
+        /// For unit testing only.
+        /// </summary>
+        public static void SetUnitTestKnownTypes(IEnumerable<Type> types)
+        {
+            _types = new List<Type>(types);
+        }
+#endif
+
+        public abstract bool IsMatch(DicomAttributeCollection dataSet);
+    }
+}
---
+++ Dicom/ServiceModel/Editing/TypeHelper.cs
@@ -0,0 +1,42 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+// 
+// For information about the licensing and copyright of this software please
+// contact ClearCanvas, Inc. at info@clearcanvas.ca
+
+#endregion
+
+using System;
+
+namespace ClearCanvas.Dicom.ServiceModel.Editing
+{
+    public static class TypeHelper
+    {
+        public static Type[] GetKnownAttributeValueTypes()
+        {
+            return new []
+                       {
+                           typeof(DateTime),
+                           typeof(DateTime?),
+                           typeof(DateTime[]),
+                           typeof(TimeSpan),
+                           typeof(TimeSpan?),
+                           typeof(TimeSpan[]),
+                           typeof(float[]),
+                           typeof(double[]),
+                           typeof(string[]),
+                           typeof(Int16[]),
+                           typeof(UInt16[]),
+                           typeof(Int32[]),
+                           typeof(UInt32[]),
+                           typeof(Int64[]),
+                           typeof(UInt64[]),
+                           typeof(byte[]),
+                           typeof(sbyte[])
+                        };
+        }
+    }
+}
