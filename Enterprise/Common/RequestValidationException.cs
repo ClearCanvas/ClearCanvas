908383445fe5ee21bc009b0871e92fd91a3c44b5
Jonathan Resnick <jonathan@clearcanvas.ca>
2013-06-18 13:42:41 -0400

    [Ticket #11142] - The research PACS shall be accessible via a REST API
    For all exceptions of type RequestValidationException, moved the message to a resx file. The reason is that RequestValidationException messages are returned directly to the client, which means that the API consumer may need to make use of the message.
    
    --HG--
    branch : Rigel

--- Enterprise/Common/RequestValidationException.cs
+++ Enterprise/Common/RequestValidationException.cs
@@ -24,18 +24,11 @@
 
 using System;
 using System.Runtime.Serialization;
-using System.ServiceModel;
-
-using ClearCanvas.Common;
-using ClearCanvas.Common.Specifications;
-using System.Collections.Generic;
-using ClearCanvas.Common.Utilities;
-using System.Threading;
 
 namespace ClearCanvas.Enterprise.Common
 {
     /// <summary>
-    /// Used by a service to indicate to the client that a request was rejected because it was invalid.  The client will likely
+    /// Used by a service to indicate to the client that a request was rejected because it was invalid.  The client may
     /// display the contained message to the end user.  Therefore, the message should be appropriate for the end-user.
     /// </summary>
     [Serializable]
