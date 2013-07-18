6d05ab7cda8931232b87b8ae8fca456729229e85
Jasper Yeh <jasper@clearcanvas.ca>
2013-07-18 15:28:24 -0400

    [Requirement #11198] - Localize all components for supported languages  * Fixed a recurring problem in the positioning of the buttons on the study filters anonymization dialog.   * The buttons will now stick to the lower right corner of the dialog.
    --HG--
    branch : Bennu

--- ImageViewer/Utilities/StudyFilters/View/WinForms/ExportComponentPanel.Designer.cs
+++ ImageViewer/Utilities/StudyFilters/View/WinForms/ExportComponentPanel.Designer.cs
@@ -45,24 +45,28 @@ namespace ClearCanvas.ImageViewer.Utilities.StudyFilters.View.WinForms {
 			// _studyDescription
 			// 
 			resources.ApplyResources(this._studyDescription, "_studyDescription");
+			this._studyDescription.Mask = "";
 			this._studyDescription.Name = "_studyDescription";
 			this._studyDescription.Value = null;
 			// 
 			// _patientId
 			// 
 			resources.ApplyResources(this._patientId, "_patientId");
+			this._patientId.Mask = "";
 			this._patientId.Name = "_patientId";
 			this._patientId.Value = null;
 			// 
 			// _accessionNumber
 			// 
 			resources.ApplyResources(this._accessionNumber, "_accessionNumber");
+			this._accessionNumber.Mask = "";
 			this._accessionNumber.Name = "_accessionNumber";
 			this._accessionNumber.Value = null;
 			// 
 			// _patientsName
 			// 
 			resources.ApplyResources(this._patientsName, "_patientsName");
+			this._patientsName.Mask = "";
 			this._patientsName.Name = "_patientsName";
 			this._patientsName.Value = null;
 			// 
@@ -101,6 +105,7 @@ namespace ClearCanvas.ImageViewer.Utilities.StudyFilters.View.WinForms {
 			// _studyId
 			// 
 			resources.ApplyResources(this._studyId, "_studyId");
+			this._studyId.Mask = "";
 			this._studyId.Name = "_studyId";
 			this._studyId.Value = null;
 			// 
