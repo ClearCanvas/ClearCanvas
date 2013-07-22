4900ed5391b11bd2cdc63073071eda9ffff986cc
Stewart Bright <stewart@clearcanvas.ca>
2013-07-22 14:08:14 -0400

    [Requirement #11286] - The user can toggle visibility of applicable overlays - Removed an unused method on an interface that broke the ImageServer build.
    --HG--
    branch : Bennu

--- ImageViewer/Layout/Basic/DisplaySetCreationOptions.cs
+++ ImageViewer/Layout/Basic/DisplaySetCreationOptions.cs
@@ -41,8 +41,6 @@ namespace ClearCanvas.ImageViewer.Layout.Basic
         bool ShowOriginalMixedMultiframeSeries { get; }
 
         bool ShowGrayscaleInverted { get; }
-
-        bool? IsOverlaySelected(string name);
     }
 
     public interface IDisplaySetCreationOptions : IEnumerable<IModalityDisplaySetCreationOptions>
