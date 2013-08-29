7edfd75034bf314f8f2539ea25e7aca47866f800
Jasper Yeh <jasper@clearcanvas.ca>
2013-08-29 11:39:36 -0400

    [Requirement #11317] - User can vary slab thickness of MPR display sets * Added more code documentation to the VolumeSliceFactory to better describe the   logic behind its implementation.
    --HG--
    branch : Rigel

--- ImageViewer/Volumes/VolumeSliceFactory.cs
+++ ImageViewer/Volumes/VolumeSliceFactory.cs
@@ -26,6 +26,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using ClearCanvas.Common;
 using ClearCanvas.ImageViewer.Mathematics;
 
 namespace ClearCanvas.ImageViewer.Volumes
@@ -42,6 +43,7 @@ namespace ClearCanvas.ImageViewer.Volumes
 		/// </summary>
 		/// <remarks>
 		/// <para>If not specified, the original row orientation of the source volume will be used.</para>
+		/// <para>In DICOM, this parameter is equivalent to the row component of the Image Position (Patient).</para>
 		/// </remarks>
 		public Vector3D RowOrientationPatient { get; set; }
 
@@ -50,6 +52,7 @@ namespace ClearCanvas.ImageViewer.Volumes
 		/// </summary>
 		/// <remarks>
 		/// <para>If not specified, the original column orientation of the source volume will be used.</para>
+		/// <para>In DICOM, this parameter is equivalent to the column component of the Image Position (Patient).</para>
 		/// </remarks>
 		public Vector3D ColumnOrientationPatient { get; set; }
 
@@ -58,6 +61,7 @@ namespace ClearCanvas.ImageViewer.Volumes
 		/// </summary>
 		/// <remarks>
 		/// <para>If not specified, a suitable isotropic spacing (i.e. <see cref="RowSpacing"/> will be the same as <see cref="ColumnSpacing"/>) will be derived based on the source volume spacing.</para>
+		/// <para>In DICOM, this parameter is equivalent to the row component of the Pixel Spacing.</para>
 		/// </remarks>
 		public float? RowSpacing { get; set; }
 
@@ -66,6 +70,7 @@ namespace ClearCanvas.ImageViewer.Volumes
 		/// </summary>
 		/// <remarks>
 		/// <para>If not specified, a suitable isotropic spacing (i.e. <see cref="ColumnSpacing"/> will be the same as <see cref="RowSpacing"/>) will be derived based on the source volume spacing.</para>
+		/// <para>In DICOM, this parameter is equivalent to the column component of the Pixel Spacing.</para>
 		/// </remarks>
 		public float? ColumnSpacing { get; set; }
 
@@ -135,33 +140,100 @@ namespace ClearCanvas.ImageViewer.Volumes
 			set { _interpolation = value; }
 		}
 
-		public VolumeSlice CreateSlice(IVolumeReference volumeReference, Vector3D startPosition)
+		/// <summary>
+		/// Creates a <see cref="VolumeSlice"/> representing the slice at the specified position in a <see cref="Volume"/>.
+		/// </summary>
+		/// <param name="volumeReference">A <see cref="IVolumeReference"/> for the <see cref="Volume"/>.</param>
+		/// <param name="position">The position, in patient coordinates, of the slice to be created.</param>
+		/// <returns>A <see cref="VolumeSlice"/> instance representing the requested slice.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="volumeReference"/> is null.</exception>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="position"/> is null.</exception>
+		public VolumeSlice CreateSlice(IVolumeReference volumeReference, Vector3D position)
 		{
-			return CreateSlicesCore(volumeReference, startPosition, null, 1).Single();
+			Platform.CheckForNullReference(volumeReference, "volumeReference");
+			Platform.CheckForNullReference(position, "position");
+
+			return CreateSlicesCore(volumeReference, position, null, 1).Single();
 		}
 
+		/// <summary>
+		/// Creates <see cref="VolumeSlice"/>s representing slices in a <see cref="Volume"/>.
+		/// </summary>
+		/// <param name="volumeReference">A <see cref="IVolumeReference"/> for the <see cref="Volume"/>.</param>
+		/// <returns>A list of <see cref="VolumeSlice"/> instances representing the slices.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="volumeReference"/> is null.</exception>
 		public IList<VolumeSlice> CreateSlices(IVolumeReference volumeReference)
 		{
-			return CreateSlicesCore(volumeReference, null, null, 0);
+			Platform.CheckForNullReference(volumeReference, "volumeReference");
+
+			return CreateSlicesCore(volumeReference, null, null, 0).ToList();
 		}
 
-		public IList<VolumeSlice> CreateSlices(IVolumeReference volumeReference, Vector3D startPosition)
+		/// <summary>
+		/// Creates <see cref="VolumeSlice"/>s representing slices around a specified position in a <see cref="Volume"/>.
+		/// </summary>
+		/// <remarks>
+		/// If extent of the slices is not fixed in any way (e.g. via specifying <see cref="Columns"/>, <see cref="SliceWidth"/>, etc.),
+		/// then <paramref name="position"/> is effectively ignored and the slices will cover the entirety of the volume.
+		/// </remarks>
+		/// <param name="volumeReference">A <see cref="IVolumeReference"/> for the <see cref="Volume"/>.</param>
+		/// <param name="position">Reference position, in patient coordinates, around which slices will be created.</param>
+		/// <returns>A list of <see cref="VolumeSlice"/> instances representing the slices.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="volumeReference"/> is null.</exception>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="position"/> is null.</exception>
+		public IList<VolumeSlice> CreateSlices(IVolumeReference volumeReference, Vector3D position)
 		{
-			return CreateSlicesCore(volumeReference, startPosition, null, 0);
+			Platform.CheckForNullReference(volumeReference, "volumeReference");
+			Platform.CheckForNullReference(position, "position");
+
+			return CreateSlicesCore(volumeReference, position, null, 0).ToList();
 		}
 
+		/// <summary>
+		/// Creates <see cref="VolumeSlice"/>s representing a number of slices starting from the specified position in a <see cref="Volume"/>.
+		/// </summary>
+		/// <param name="volumeReference">A <see cref="IVolumeReference"/> for the <see cref="Volume"/>.</param>
+		/// <param name="startPosition">Position, in patient coordinates, of the first slice.</param>
+		/// <param name="count">The number of slices to be created.</param>
+		/// <returns>A list of <see cref="VolumeSlice"/> instances representing the slices.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="volumeReference"/> is null.</exception>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="startPosition"/> is null.</exception>
+		/// <exception cref="ArgumentException">Thrown if <paramref name="count"/> is less than or equal to zero.</exception>
+		public IList<VolumeSlice> CreateSlices(IVolumeReference volumeReference, Vector3D startPosition, int count)
+		{
+			Platform.CheckForNullReference(volumeReference, "volumeReference");
+			Platform.CheckForNullReference(startPosition, "startPosition");
+			Platform.CheckPositive(count, "count");
+
+			return CreateSlicesCore(volumeReference, startPosition, null, count).ToList();
+		}
+
+		/// <summary>
+		/// Creates <see cref="VolumeSlice"/>s representing slices between two positions in a <see cref="Volume"/>.
+		/// </summary>
+		/// <param name="volumeReference">A <see cref="IVolumeReference"/> for the <see cref="Volume"/>.</param>
+		/// <param name="startPosition">Position, in patient coordinates, of the first slice.</param>
+		/// <param name="endPosition">Position, in patient coordinates, of the last slice.</param>
+		/// <returns>A list of <see cref="VolumeSlice"/> instances representing the slices.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="volumeReference"/> is null.</exception>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="startPosition"/> is null.</exception>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="endPosition"/> is null.</exception>
 		public IList<VolumeSlice> CreateSlices(IVolumeReference volumeReference, Vector3D startPosition, Vector3D endPosition)
 		{
-			return CreateSlicesCore(volumeReference, startPosition, endPosition, 0);
+			Platform.CheckForNullReference(volumeReference, "volumeReference");
+			Platform.CheckForNullReference(startPosition, "startPosition");
+			Platform.CheckForNullReference(endPosition, "endPosition");
+
+			return CreateSlicesCore(volumeReference, startPosition, endPosition, 0).ToList();
 		}
 
-		private IList<VolumeSlice> CreateSlicesCore(IVolumeReference volumeReference, Vector3D startPosition, Vector3D endPosition, int count)
+		private IEnumerable<VolumeSlice> CreateSlicesCore(IVolumeReference volumeReference, Vector3D startPosition, Vector3D endPosition, int count)
 		{
 			// get the pixel spacing (defaults to isotropic spacing based on smallest volume spacing dimension)
 			var pixelSpacing = GetPixelSpacing(volumeReference);
 
 			// get the spacing between slices (defaults to smallest volume spacing dimension)
-			var sliceSpacing = SliceSpacing ?? GetMinimumComponent(volumeReference.VoxelSpacing);
+			var sliceSpacing = GetSliceSpacing(volumeReference);
 
 			// get the thickness of each slice (defaults to slice spacing)
 			var sliceThickness = SliceThickness ?? sliceSpacing;
@@ -215,56 +287,81 @@ namespace ClearCanvas.ImageViewer.Volumes
 			// capture all the slicer parameters in an args object
 			var args = new VolumeSliceArgs(sliceRows, sliceColumns, pixelSpacing.Height, pixelSpacing.Width, slicerAxisX, slicerAxisY, sliceThickness, Interpolation);
 
-			// get the number of slices in the output
-			var sliceCount = (int) (Math.Abs(1.0*stackDepth/sliceSpacing) + 0.5);
+			// compute the image position patient for each output slice and create the slices
+			return GetSlicePositions(slicerOrigin, slicerAxisX, slicerAxisY, slicerAxisZ, stackDepth, sliceRows, sliceColumns, pixelSpacing, sliceSpacing, startPosition, endPosition, count)
+				.Select(p => new VolumeSlice(volumeReference.Clone(), true, args, p, sliceSpacing));
+		}
 
+		private IEnumerable<Vector3D> GetSlicePositions(Vector3D slicerOrigin, Vector3D slicerAxisX, Vector3D slicerAxisY, Vector3D slicerAxisZ, float stackDepth, int sliceRows, int sliceColumns, SizeF pixelSpacing, float sliceSpacing, Vector3D startPosition, Vector3D endPosition, int count)
+		{
 			// compute the increment in position between consecutive slices
 			var slicePositionIncrement = slicerAxisZ*sliceSpacing;
 
-			// compute the position of the first slice
+			// get the number of slices in the output when slicing the entire volume
+			var sliceCount = (int) (Math.Abs(1.0*stackDepth/sliceSpacing) + 0.5);
+
+			// compute the position of the first slice when slicing the entire volume
 			var firstSlicePosition = slicerOrigin;
 
+			// if a start position (and, optionally, end position or total count) is provided, we adjust the slice position and count accordingly
 			if (startPosition != null)
 			{
+				// NOTE: The overall logic here is that, even if a position is given for the first slice, it does not make sense to arbitrarily
+				// reslice from that point to the edge of the volume, because the other half of the volume will not be in the output.
+				// Therefore, we only make use of the individual components of the position for which the client code has provided fixed bounds
+				// because then the client code is explicitly requested a subrange of the volume in that dimension, and any auto-calculated
+				// bounds will still include the entire volume in that dimension.
+				// e.g. we use the X component of the position if the output width is fixed, and the output stack will be horizontally centred on this position
+
+				// get the requested start position in the slicer frame
 				var slicerStart = startPosition - slicerOrigin;
 
+				// if the width of the output is fixed, we horizontally centre the slices on the location provided
 				if (Columns.HasValue || SliceWidth.HasValue)
 				{
+					// projecting the start position on to the X axis then subtracting half the width of the image gives us an offset along X to get desired starting location
 					var offsetX = slicerAxisX.Dot(slicerStart) - sliceColumns*pixelSpacing.Width/2f;
 					firstSlicePosition += offsetX*slicerAxisX;
 				}
 
+				// if the height of the output is fixed, we vertically centre the slices on the location provided
 				if (Rows.HasValue || SliceHeight.HasValue)
 				{
+					// projecting the start position on to the Y axis then subtracting half the width of the image gives us an offset along Y to get desired starting location
 					var offsetY = slicerAxisY.Dot(slicerStart) - sliceRows*pixelSpacing.Height/2f;
 					firstSlicePosition += offsetY*slicerAxisY;
 				}
 
+				// if end position or total slice count is provided, we will slice the selected range only
 				if (endPosition != null)
 				{
+					// projecting the start position on to the Z axis gives us an offset along Z to get desired starting location
 					var offsetZ = slicerAxisZ.Dot(slicerStart);
 					firstSlicePosition += offsetZ*slicerAxisZ;
 
-					var stackRange = slicerAxisZ.Dot(endPosition - slicerOrigin) - offsetZ;
+					// recompute the slice count based on the magnitude of the Z component delta between the end and start positions
+					var slicerStop = endPosition - slicerOrigin;
+					var stackRange = slicerAxisZ.Dot(slicerStop) - offsetZ;
 					sliceCount = (int) (Math.Abs(1.0*stackRange/sliceSpacing) + 0.5);
 				}
 				else if (count > 0)
 				{
+					// projecting the start position on to the Z axis gives us an offset along Z to get desired starting location
 					var offsetZ = slicerAxisZ.Dot(slicerStart);
 					firstSlicePosition += offsetZ*slicerAxisZ;
 
+					// the slice count is exactly as provided
 					sliceCount = count;
 				}
 			}
 
-			// create the output slices
-			var list = new List<VolumeSlice>();
-			for (var n = 0; n < sliceCount; ++n)
-			{
-				var imagePositionPatient = firstSlicePosition + n*slicePositionIncrement;
-				list.Add(new VolumeSlice(volumeReference.Clone(), args, imagePositionPatient, sliceSpacing));
-			}
-			return list;
+			// compute the positions for the requested slices
+			return Enumerable.Range(0, sliceCount).Select(n => firstSlicePosition + n*slicePositionIncrement);
+		}
+
+		private float GetSliceSpacing(IVolumeHeader volumeReference)
+		{
+			return SliceSpacing ?? GetMinimumComponent(volumeReference.VoxelSpacing);
 		}
 
 		private SizeF GetPixelSpacing(IVolumeHeader volumeHeader)
