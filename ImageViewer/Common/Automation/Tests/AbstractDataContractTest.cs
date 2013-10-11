8bfe2646db0090679892aca68f7f0853e376cbd7
Jasper Yeh <jasper@clearcanvas.ca>
2013-10-11 13:59:13 -0400

    [Bug #11578] ImageViewer automation no longer working in 10.1 * Reorganized unit tests.  * Added unit tests for all the other data contracts as they existed in the    2.0 release. * Added an additional unit test to verify that the legacy UID field on the   Viewer contract will interact properly with the identifier field if one or   both are set.
    --HG--
    branch : Bennu
    rename : ImageViewer/Common/Automation/Tests/DataContractTests.cs => ImageViewer/Common/Automation/Tests/AbstractDataContractTest.cs

--- ImageViewer/Common/Automation/Tests/AbstractDataContractTest.cs
+++ ImageViewer/Common/Automation/Tests/AbstractDataContractTest.cs
@@ -29,78 +29,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using ClearCanvas.Common;
-using ClearCanvas.Dicom.ServiceModel.Query;
 using NUnit.Framework;
 
 namespace ClearCanvas.ImageViewer.Common.Automation.Tests
 {
-	[TestFixture]
-	internal class DataContractTests
+	internal abstract class AbstractDataContractTest
 	{
-		[Test]
-		public void TestOpenStudiesRequestContractCompatibility1()
-		{
-			var referenceObject = new LegacyOpenStudiesRequestCirca20
-			                      	{
-			                      		ActivateIfAlreadyOpen = false,
-			                      		StudiesToOpen = new List<OpenStudyInfo>
-			                      		                	{
-			                      		                		new OpenStudyInfo("1.234"),
-			                      		                		new OpenStudyInfo("56.78"),
-			                      		                		new OpenStudyInfo("9.0")
-			                      		                	}
-			                      	};
-
-			var currentObject = TestRequestContractCompatibility<OpenStudiesRequest>(referenceObject);
-			Assert.AreEqual(referenceObject.ActivateIfAlreadyOpen, currentObject.ActivateIfAlreadyOpen);
-			AssertAreSequenceEqual(referenceObject.StudiesToOpen, currentObject.StudiesToOpen, (a, b) => a.StudyInstanceUid == b.StudyInstanceUid);
-		}
-
-		[Test]
-		public void TestOpenStudiesRequestContractCompatibility2()
-		{
-			var referenceObject = new LegacyOpenStudiesRequestCirca30
-			                      	{
-			                      		ActivateIfAlreadyOpen = false,
-			                      		ReportFaultToUser = true,
-			                      		StudiesToOpen = new List<OpenStudyInfo>
-			                      		                	{
-			                      		                		new OpenStudyInfo("1.234"),
-			                      		                		new OpenStudyInfo("56.78"),
-			                      		                		new OpenStudyInfo("9.0")
-			                      		                	}
-			                      	};
-
-			var currentObject = TestRequestContractCompatibility<OpenStudiesRequest>(referenceObject);
-			Assert.AreEqual(referenceObject.ActivateIfAlreadyOpen, currentObject.ActivateIfAlreadyOpen);
-			Assert.AreEqual(referenceObject.ReportFaultToUser, currentObject.ReportFaultToUser);
-			AssertAreSequenceEqual(referenceObject.StudiesToOpen, currentObject.StudiesToOpen, (a, b) => a.StudyInstanceUid == b.StudyInstanceUid);
-		}
-
-		[Test]
-		public void TestViewerContractCompatibility1()
-		{
-			var referenceObject = new Viewer
-			                      	{
-			                      		Identifier = Guid.NewGuid(),
-			                      		PrimaryStudyIdentifier = new StudyRootStudyIdentifier
-			                      		                         	{
-			                      		                         		AccessionNumber = "ACC",
-			                      		                         		StudyInstanceUid = "123.456"
-			                      		                         	}
-			                      	};
-
-			var legacyObject = TestResponseContractCompatibility<LegacyViewerPre90>(referenceObject);
-			Assert.AreEqual(referenceObject.Identifier, legacyObject.Identifier);
-			Assert.AreEqual(referenceObject.PrimaryStudyInstanceUid, legacyObject.PrimaryStudyInstanceUid);
-
-			var currentObject = TestRequestContractCompatibility<Viewer>(legacyObject);
-			Assert.AreEqual(referenceObject.Identifier, currentObject.Identifier);
-			Assert.AreEqual(referenceObject.PrimaryStudyInstanceUid, currentObject.PrimaryStudyInstanceUid);
-			Assert.IsNotNull(currentObject.PrimaryStudyIdentifier);
-			Assert.AreEqual(referenceObject.PrimaryStudyIdentifier.StudyInstanceUid, currentObject.PrimaryStudyIdentifier.StudyInstanceUid);
-		}
-
 		/// <summary>
 		/// Used for testing backward compatibility of current response contracts sent to legacy clients.
 		/// </summary>
@@ -108,11 +42,12 @@ namespace ClearCanvas.ImageViewer.Common.Automation.Tests
 		/// <param name="currentObject">The object to test using the current data contract type.</param>
 		/// <param name="dumpSerializedData">Specifies whether or not the serialized data should be dumped to the console even if errors do not occur during serialization.</param>
 		/// <returns>The equivalent of <paramref name="currentObject"/> deserialized as a <typeparamref name="TLegacy"/>.</returns>
-		private static TLegacy TestResponseContractCompatibility<TLegacy>(object currentObject, bool dumpSerializedData = false)
+		protected static TLegacy TestResponseContractCompatibility<TLegacy>(object currentObject, bool dumpSerializedData = false)
 			where TLegacy : class
 		{
 			Platform.CheckForNullReference(currentObject, "currentObject");
 			Platform.CheckTrue(currentObject.GetType() != typeof (TLegacy), "currentObject cannot be of type TLegacy (otherwise, it wouldn't be a valid test!)");
+			Platform.CheckTrue((typeof (TLegacy).Namespace ?? string.Empty).StartsWith("ClearCanvas.ImageViewer.Common.Automation.Tests"), "TLegacy doesn't appear to be a legacy data contract defined for the sole purpose of unit tests!");
 
 			var serializedData = string.Empty;
 			try
@@ -153,11 +88,12 @@ namespace ClearCanvas.ImageViewer.Common.Automation.Tests
 		/// <param name="legacyObject">The object to test using the legacy data contract type.</param>
 		/// <param name="dumpSerializedData">Specifies whether or not the serialized data should be dumped to the console even if errors do not occur during serialization.</param>
 		/// <returns>The equivalent of <paramref name="legacyObject"/> deserialized as a <typeparamref name="TCurrent"/>.</returns>
-		private static TCurrent TestRequestContractCompatibility<TCurrent>(object legacyObject, bool dumpSerializedData = false)
+		protected static TCurrent TestRequestContractCompatibility<TCurrent>(object legacyObject, bool dumpSerializedData = false)
 			where TCurrent : class
 		{
 			Platform.CheckForNullReference(legacyObject, "legacyObject");
-			Platform.CheckTrue(legacyObject.GetType() != typeof (TCurrent), "legacyObject cannot be of type TLegacy (otherwise, it wouldn't be a valid test!)");
+			Platform.CheckTrue(legacyObject.GetType() != typeof (TCurrent), "legacyObject cannot be of type TCurrent (otherwise, it wouldn't be a valid test!)");
+			Platform.CheckFalse((typeof (TCurrent).Namespace ?? string.Empty).StartsWith("ClearCanvas.ImageViewer.Common.Automation.Tests"), "TCurrent appears to be a legacy data contract defined for the sole purpose of unit tests!");
 
 			var serializedData = string.Empty;
 			try
@@ -191,7 +127,7 @@ namespace ClearCanvas.ImageViewer.Common.Automation.Tests
 			}
 		}
 
-		private static void AssertAreSequenceEqual<TLeft, TRight>(IList<TLeft> expected, IList<TRight> actual, Func<TLeft, TRight, bool> comparer, string message = null, params object[] args)
+		protected static void AssertAreSequenceEqual<TLeft, TRight>(IList<TLeft> expected, IList<TRight> actual, Func<TLeft, TRight, bool> comparer, string message = null, params object[] args)
 		{
 			if (ReferenceEquals(expected, null))
 			{
---
+++ ImageViewer/Common/Automation/Tests/BaselineDataContracts.cs
@@ -0,0 +1,231 @@
+﻿#region License
+
+// Copyright (c) 2013, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This file is part of the ClearCanvas RIS/PACS open source project.
+//
+// The ClearCanvas RIS/PACS open source project is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// The ClearCanvas RIS/PACS open source project is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the ClearCanvas RIS/PACS open source project.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#if UNIT_TESTS
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ClearCanvas.ImageViewer.Common.Automation.Tests
+{
+	/// <summary>
+	/// Legacy data contract definitions as they existed at baseline (version 2.0)
+	/// </summary>
+	internal static class BaselineDataContracts
+	{
+		private const string _namespace = "http://www.clearcanvas.ca/imageViewer/automation";
+
+		[DataContract(Name = "NoActiveViewersFault", Namespace = _namespace)]
+		public class NoActiveViewersFault {}
+
+		[DataContract(Name = "NoViewersFault", Namespace = _namespace)]
+		public class NoViewersFault {}
+
+		[DataContract(Name = "ViewerNotFoundFault", Namespace = _namespace)]
+		public class ViewerNotFoundFault
+		{
+			[DataMember(IsRequired = false)]
+			public string FailureDescription { get; set; }
+		}
+
+		[DataContract(Name = "OpenStudiesFault", Namespace = _namespace)]
+		public class OpenStudiesFault
+		{
+			[DataMember(IsRequired = false)]
+			public string FailureDescription { get; set; }
+		}
+
+		[DataContract(Name = "OpenFilesFault", Namespace = _namespace)]
+		public class OpenFilesFault
+		{
+			[DataMember(IsRequired = false)]
+			public string FailureDescription { get; set; }
+		}
+
+		[DataContract(Name = "Viewer", Namespace = _namespace)]
+		public class Viewer
+		{
+			[DataMember(IsRequired = true)]
+			public Guid Identifier { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public string PrimaryStudyInstanceUid { get; set; }
+		}
+
+		[DataContract(Name = "GetActiveViewersResult", Namespace = _namespace)]
+		public class GetActiveViewersResult
+		{
+			[DataMember(IsRequired = true)]
+			public List<Viewer> ActiveViewers { get; set; }
+		}
+
+		[DataContract(Name = "GetViewersRequest", Namespace = _namespace)]
+		public class GetViewersRequest {}
+
+		[DataContract(Name = "GetViewersResult", Namespace = _namespace)]
+		public class GetViewersResult
+		{
+			[DataMember(IsRequired = true)]
+			public List<Viewer> Viewers { get; set; }
+		}
+
+		[DataContract(Name = "GetViewerInfoRequest", Namespace = _namespace)]
+		public class GetViewerInfoRequest
+		{
+			[DataMember(IsRequired = true)]
+			public Viewer Viewer { get; set; }
+		}
+
+		[DataContract(Name = "GetViewerInfoResult", Namespace = _namespace)]
+		public class GetViewerInfoResult
+		{
+			[DataMember(IsRequired = true)]
+			public List<string> AdditionalStudyInstanceUids { get; set; }
+		}
+
+		[DataContract(Name = "OpenStudiesResult", Namespace = _namespace)]
+		public class OpenStudiesResult
+		{
+			[DataMember(IsRequired = true)]
+			public Viewer Viewer { get; set; }
+		}
+
+		[DataContract(Name = "OpenStudyInfo", Namespace = _namespace)]
+		public class OpenStudyInfo
+		{
+			[DataMember(IsRequired = true)]
+			public string StudyInstanceUid { get; set; }
+
+			[DataMember(IsRequired = false)]
+			public string SourceAETitle { get; set; }
+		}
+
+		[DataContract(Name = "OpenFilesRequest", Namespace = _namespace)]
+		public class OpenFilesRequest
+		{
+			[DataMember(IsRequired = true)]
+			public List<string> Files { get; set; }
+
+			[DataMember(IsRequired = false)]
+			public bool? WaitForFilesToOpen { get; set; }
+
+			[DataMember(IsRequired = false)]
+			public bool? ReportFaultToUser { get; set; }
+		}
+
+		[DataContract(Name = "OpenFilesResult", Namespace = _namespace)]
+		public class OpenFilesResult
+		{
+			[DataMember]
+			public Viewer Viewer { get; set; }
+		}
+
+		[DataContract(Name = "OpenStudiesRequest", Namespace = _namespace)]
+		public class OpenStudiesRequest
+		{
+			[DataMember(IsRequired = true)]
+			public List<OpenStudyInfo> StudiesToOpen { get; set; }
+
+			[DataMember(IsRequired = false)]
+			public bool? ActivateIfAlreadyOpen { get; set; }
+		}
+
+		[DataContract(Name = "CloseViewerRequest", Namespace = _namespace)]
+		public class CloseViewerRequest
+		{
+			[DataMember(IsRequired = true)]
+			public Viewer Viewer { get; set; }
+		}
+
+		[DataContract(Name = "ActivateViewerRequest", Namespace = _namespace)]
+		public class ActivateViewerRequest
+		{
+			[DataMember(IsRequired = true)]
+			public Viewer Viewer { get; set; }
+		}
+
+		[DataContract(Name = "DicomExplorerNotFoundFault", Namespace = _namespace)]
+		public class DicomExplorerNotFoundFault {}
+
+		[DataContract(Name = "ServerNotFoundFault", Namespace = _namespace)]
+		public class ServerNotFoundFault {}
+
+		[DataContract(Name = "NoLocalStoreFault", Namespace = _namespace)]
+		public class NoLocalStoreFault {}
+
+		[DataContract(Name = "DicomExplorerSearchCriteria", Namespace = _namespace)]
+		public class DicomExplorerSearchCriteria
+		{
+			[DataMember(IsRequired = true)]
+			public DateTime? StudyDateFrom { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public DateTime? StudyDateTo { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public string PatientId { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public string PatientsName { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public string AccessionNumber { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public string StudyDescription { get; set; }
+
+			[DataMember(IsRequired = false)]
+			public string ReferringPhysiciansName { get; set; }
+
+			[DataMember(IsRequired = true)]
+			public List<string> Modalities { get; set; }
+		}
+
+		[DataContract(Name = "SearchStudiesRequest", Namespace = _namespace)]
+		public abstract class SearchStudiesRequest
+		{
+			[DataMember(IsRequired = true)]
+			public DicomExplorerSearchCriteria SearchCriteria { get; set; }
+		}
+
+		[DataContract(Name = "SearchLocalStudiesRequest", Namespace = _namespace)]
+		public class SearchLocalStudiesRequest : SearchStudiesRequest {}
+
+		[DataContract(Name = "SearchLocalStudiesResult", Namespace = _namespace)]
+		public class SearchLocalStudiesResult {}
+
+		[DataContract(Name = "SearchRemoteStudiesRequest", Namespace = _namespace)]
+		public class SearchRemoteStudiesRequest : SearchStudiesRequest
+		{
+			[DataMember(IsRequired = true)]
+			public string AETitle { get; set; }
+		}
+
+		[DataContract(Name = "SearchRemoteStudiesResult", Namespace = _namespace)]
+		public class SearchRemoteStudiesResult {}
+	}
+}
+
+#endif
