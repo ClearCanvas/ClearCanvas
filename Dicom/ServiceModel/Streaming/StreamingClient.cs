81d3fb73dbb4392104e15fa7c1bdc24b2d1390e5
Stewart Bright <stewart@clearcanvas.ca>
2013-10-29 22:45:36 -0400

    [Requirement #11516] - Viewing PDF Reports in WebstationDX - Moved Attachments (Web) code into it's own plugin. - Refactored StreamingClient a bit so the download URL is created via a single method. Added a generic GetImageData method that can download image data via the WADO service in an arbitrary format. - Wrote a generic SopFormatHandler, which is a proxy onto the WADO service (currently), but serves up the content on standard ports. Tested using the handler with both PDF and JPG formats, but disabled the JPG one for now. - Fixed JpegMimeType WADO processor to dispose the viewer objects it creates. - Added ViewPdf.aspx page in Studies/ViewHtml folder, which shows the PDF inside an IS master page border. The Webstation attachments shelf opens a new window with this as the URL. - Added "baseUrl" to Webstation application context.
    --HG--
    branch : Rigel

--- Dicom/ServiceModel/Streaming/StreamingClient.cs
+++ Dicom/ServiceModel/Streaming/StreamingClient.cs
@@ -79,6 +79,29 @@ namespace ClearCanvas.Dicom.ServiceModel.Streaming
 		}
 	}
 
+	public struct StreamingClientArgs
+	{
+		public StreamingClientArgs(string serverAE, string studyInstanceUid, string seriesInstanceUid, string sopInstanceUid)
+		{
+			ServerAE = serverAE;
+			StudyInstanceUid = studyInstanceUid;
+			SeriesInstanceUid = seriesInstanceUid;
+			SopInstanceUid = sopInstanceUid;
+			FrameNumber = null;
+			ContentType = null;
+			StopTag = null;
+		}
+
+		public readonly string ServerAE;
+		public readonly string StudyInstanceUid;
+		public readonly string SeriesInstanceUid;
+		public readonly string SopInstanceUid;
+		
+		public uint? StopTag;
+		public int? FrameNumber;
+		public string ContentType;
+	}
+
     /// <summary>
     /// Represents a web client that can be used to retrieve study images or pixel data from a streaming server using WADO protocol.
     /// </summary>
@@ -105,24 +128,12 @@ namespace ClearCanvas.Dicom.ServiceModel.Streaming
 				clock.Start();
 
 				FrameStreamingResultMetaData result = new FrameStreamingResultMetaData();
-				StringBuilder url = new StringBuilder();
-
-				if (_baseUri.ToString().EndsWith("/"))
-				{
-					url.AppendFormat("{0}{1}", _baseUri, serverAE);
-				}
-				else
-				{
-					url.AppendFormat("{0}/{1}", _baseUri, serverAE);
-				}
-
-				url.AppendFormat("?requesttype=WADO&studyUID={0}&seriesUID={1}&objectUID={2}", studyInstanceUID, seriesInstanceUID, sopInstanceUid);
-				url.AppendFormat("&frameNumber={0}", frame);
-				url.AppendFormat("&contentType={0}", HttpUtility.HtmlEncode("application/clearcanvas"));
 
 				result.Speed.Start();
 
-				HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url.ToString());
+				var args = new StreamingClientArgs(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid){FrameNumber = frame};
+				var downloadUri = GetDownloadUri(args);
+				HttpWebRequest request = (HttpWebRequest) WebRequest.Create(downloadUri.AbsoluteUri);
 				request.Accept = "application/dicom,application/clearcanvas,image/jpeg";
 				request.Timeout = (int) TimeSpan.FromSeconds(StreamingSettings.Default.ClientTimeoutSeconds).TotalMilliseconds;
 				request.KeepAlive = false;
@@ -181,10 +192,9 @@ namespace ClearCanvas.Dicom.ServiceModel.Streaming
 
 		public Stream RetrieveImageHeader(string serverAE, string studyInstanceUID, string seriesInstanceUID, string sopInstanceUid, uint stopTag, out StreamingResultMetaData metaInfo)
 		{
-        	string imageUrl = BuildImageUrl(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid);
-			imageUrl = imageUrl + String.Format("&stopTag={0:x8}", stopTag);
-			imageUrl = imageUrl + String.Format("&contentType={0}", HttpUtility.HtmlEncode("application/clearcanvas-header"));
-        	return RetrieveImageData(imageUrl, out metaInfo);
+			var args = new StreamingClientArgs(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid) {ContentType = "application/clearcanvas-header", StopTag = stopTag};
+			var imageUrl = GetDownloadUri(args);
+        	return RetrieveImageData(imageUrl.AbsoluteUri, out metaInfo);
 		}
 		
 		public Stream RetrieveImage(string serverAE, string studyInstanceUID, string seriesInstanceUID, string sopInstanceUid)
@@ -195,9 +205,9 @@ namespace ClearCanvas.Dicom.ServiceModel.Streaming
 
         public Stream RetrieveImage(string serverAE, string studyInstanceUID, string seriesInstanceUID, string sopInstanceUid, out StreamingResultMetaData metaInfo)
         {
-        	string imageUrl = BuildImageUrl(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid);
-			imageUrl = imageUrl + String.Format("&contentType={0}", HttpUtility.HtmlEncode("application/dicom"));
-        	return RetrieveImageData(imageUrl, out metaInfo);
+			var args = new StreamingClientArgs(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid) { ContentType = "application/dicom"};
+			var imageUrl = GetDownloadUri(args);
+        	return RetrieveImageData(imageUrl.AbsoluteUri, out metaInfo);
         }
 
         public Stream RetrievePdf(string serverAE, string studyInstanceUID, string seriesInstanceUID, string sopInstanceUid)
@@ -208,27 +218,48 @@ namespace ClearCanvas.Dicom.ServiceModel.Streaming
 
         public Stream RetrievePdf(string serverAE, string studyInstanceUID, string seriesInstanceUID, string sopInstanceUid, out StreamingResultMetaData metaInfo)
         {
-            string imageUrl = BuildImageUrl(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid);
-            imageUrl = imageUrl + String.Format("&contentType={0}", HttpUtility.HtmlEncode("application/pdf"));
-            return RetrieveImageData(imageUrl, out metaInfo);
+			var args = new StreamingClientArgs(serverAE, studyInstanceUID, seriesInstanceUID, sopInstanceUid) { ContentType = "application/pdf" };
+			var imageUrl = GetDownloadUri(args);
+            return RetrieveImageData(imageUrl.AbsoluteUri, out metaInfo);
         }
+
+		public Stream RetrieveImageData(StreamingClientArgs args)
+		{
+			var uri = GetDownloadUri(args);
+			StreamingResultMetaData result;
+			return RetrieveImageData(uri.AbsoluteUri, out result);
+		}
+
+		public Stream RetrieveImageData(StreamingClientArgs args, out StreamingResultMetaData result)
+		{
+			var uri = GetDownloadUri(args);
+			return RetrieveImageData(uri.AbsoluteUri, out result);
+		}
 
         #endregion Public Methods
 
 		#region Private Methods
 
-		private string BuildImageUrl(string serverAE, string studyInstanceUid, string seriesInstanceUid, string sopInstanceUid)
+		private Uri GetDownloadUri(StreamingClientArgs args)
 		{
-			Platform.CheckForEmptyString(serverAE, "serverAE");
-			Platform.CheckForEmptyString(studyInstanceUid, "studyInstanceUid");
-			Platform.CheckForEmptyString(seriesInstanceUid, "seriesInstanceUid");
-			Platform.CheckForEmptyString(sopInstanceUid, "sopInstanceUid");
+			Platform.CheckForEmptyString(args.ServerAE, "serverAE");
+			Platform.CheckForEmptyString(args.StudyInstanceUid, "studyInstanceUid");
+			Platform.CheckForEmptyString(args.SeriesInstanceUid, "seriesInstanceUid");
+			Platform.CheckForEmptyString(args.SopInstanceUid, "sopInstanceUid");
 
 			var url = new StringBuilder();
-		    url.AppendFormat(_baseUri.ToString().EndsWith("/") ? "{0}{1}" : "{0}/{1}", _baseUri, serverAE);
+			url.AppendFormat(_baseUri.ToString().EndsWith("/") ? "{0}{1}" : "{0}/{1}", _baseUri, args.ServerAE);
 
-		    url.AppendFormat("?requesttype=WADO&studyUID={0}&seriesUID={1}&objectUID={2}", studyInstanceUid, seriesInstanceUid, sopInstanceUid);
-			return url.ToString();
+			url.AppendFormat("?requesttype=WADO&studyUID={0}&seriesUID={1}&objectUID={2}", args.StudyInstanceUid, args.SeriesInstanceUid, args.SopInstanceUid);
+
+			if (args.FrameNumber.HasValue)
+				url.AppendFormat("&frameNumber={0}", args.FrameNumber.Value);
+			if (args.StopTag.HasValue)
+				url.AppendFormat("&stopTag={0:x8}", args.StopTag.Value);
+			if (!String.IsNullOrEmpty(args.ContentType))
+				url.AppendFormat("&contentType={0}", HttpUtility.HtmlEncode(args.ContentType));
+
+			return new Uri(url.ToString());
 		}
 
 		private static MemoryStream RetrieveImageData(string url, out StreamingResultMetaData result)
@@ -239,7 +270,7 @@ namespace ClearCanvas.Dicom.ServiceModel.Streaming
 
 				result.Speed.Start();
 
-				var request = (HttpWebRequest) WebRequest.Create(url.ToString());
+				var request = (HttpWebRequest) WebRequest.Create(url);
 				request.Accept = "application/dicom,application/clearcanvas,application/clearcanvas-header,image/jpeg,application/pdf";
 				request.Timeout = (int) TimeSpan.FromSeconds(StreamingSettings.Default.ClientTimeoutSeconds).TotalMilliseconds;
 				request.KeepAlive = false;
---
+++ ImageServer/Web/Common/Modules/SopFormatHandler.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Security.Authentication;
+using System.Security.Permissions;
+using System.Threading;
+using System.Web;
+using ClearCanvas.Common;
+using ClearCanvas.Dicom.ServiceModel.Streaming;
+using ClearCanvas.Enterprise.Common;
+using ClearCanvas.ImageServer.Core;
+using ClearCanvas.ImageServer.Model;
+using ClearCanvas.ImageServer.Web.Common.Security;
+
+namespace ClearCanvas.ImageServer.Web.Common.Modules
+{
+	public class PdfSopFormatHandler : SopFormatHandler, IHttpHandler
+	{
+		#region Overrides of ViewSopHandlerBase
+
+		protected override string Name
+		{
+			get { return "View PDF"; }
+		}
+
+		protected override string ContentType
+		{
+			get { return "application/pdf"; }
+		}
+
+		#endregion
+	}
+	/*
+	public class JpgSopFormatHandler : SopFormatHandler, IHttpHandler
+	{
+		#region Overrides of ViewSopHandlerBase
+
+		protected override string Name
+		{
+			get { return "View JPG"; }
+		}
+
+		protected override string ContentType
+		{
+			get { return "image/jpeg"; }
+		}
+
+		#endregion
+	}*/
+
+	public abstract class SopFormatHandler
+	{
+		private const string StudyUrl = "study=";
+		private const string SeriesUrl = "series=";
+		private const string SopUrl = "sop=";
+		private const string AeTitleUrl = "ae=";
+
+		protected abstract string Name { get; }
+		protected abstract string ContentType { get; }
+
+		public void ProcessRequest(HttpContext context)
+		{
+			Platform.Log(LogLevel.Debug, "{0}: Received request from {1}", Name, context.Request.UserHostAddress);
+			if (SessionManager.Current == null)
+				throw new UserAccessDeniedException();
+			if (!SessionManager.Current.User.IsInRole(ImageServer.Common.Authentication.AuthorityTokens.Study.View))
+				throw new UserAccessDeniedException();
+			
+			string url = context.Request.RawUrl;
+
+			string studyInstanceUid = string.Empty;
+			string aeTitle = string.Empty;
+			string seriesInstanceUid = string.Empty;
+			string sopInstanceUid = string.Empty;
+
+			string[] strings = url.Split(new[] { '/', '?', '&' });
+
+			foreach (string s in strings)
+			{
+				if (s.StartsWith(StudyUrl))
+				{
+					studyInstanceUid = s.Substring(StudyUrl.Length);
+				}
+				if (s.StartsWith(SeriesUrl))
+				{
+					seriesInstanceUid = s.Substring(SeriesUrl.Length);
+				}
+				if (s.StartsWith(SopUrl))
+				{
+					sopInstanceUid = s.Substring(SopUrl.Length);
+				}
+				if (s.StartsWith(AeTitleUrl))
+				{
+					aeTitle = s.Substring(AeTitleUrl.Length);
+				}
+			}
+
+			if (string.IsNullOrEmpty(studyInstanceUid) || string.IsNullOrEmpty(seriesInstanceUid) || string.IsNullOrEmpty(sopInstanceUid) || string.IsNullOrEmpty(aeTitle))
+			{
+				Platform.Log(LogLevel.Debug, "{0}: request does not contain all required parameters: {1}", Name, url);
+
+				//TODO: redirect to error page?
+				return;
+			}
+
+			ServerPartition ae = ServerPartitionMonitor.Instance.GetPartition(aeTitle);
+			if (ae == null)
+			{
+				//TODO: redirect to error page?
+				Platform.Log(LogLevel.Debug, "{0}: Unexpected incorrect server information.", Name);
+				throw new Exception("Unexpected incorrect server information.");
+			}
+
+			var uri = new Uri(string.Format("http://{0}:{1}/wado", "localhost", 1000));
+
+			try
+			{
+				Platform.Log(LogLevel.Debug, "{0}: Retrieving data from {0}", Name, uri);
+
+				var client = new StreamingClient(uri);
+				var args = new StreamingClientArgs(aeTitle, studyInstanceUid, seriesInstanceUid, sopInstanceUid) {ContentType = ContentType};
+				Stream input = client.RetrieveImageData(args);
+
+				if (input == null)
+				{
+					Platform.Log(LogLevel.Debug, "{0}: server did not return any data", Name);
+				}
+
+				context.Response.ContentType = ContentType;
+
+				var buffer = new byte[64 * 1024];
+				using (var ms = new MemoryStream())
+				{
+					int read;
+					while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						ms.Write(buffer, 0, read);
+					}
+					context.Response.BinaryWrite(ms.ToArray());
+				}
+			}
+			catch (StreamingClientException ex)
+			{
+				Platform.Log(LogLevel.Error, ex, "{0}:  Error occurred while retrieving report", Name);
+
+				//TODO: redirect to error page?
+				throw;
+			}
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return true;
+			}
+		}
+	}
+}
