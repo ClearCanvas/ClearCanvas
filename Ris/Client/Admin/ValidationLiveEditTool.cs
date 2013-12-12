cbf1513cad754c44dbd888fd392faae995056bcd
Jay Liu <jay@clearcanvas.ca>
2013-12-12 15:05:31 -0500

    [Ticket #11119] - Ensure the build script repository is managed properly
    - move the RIS.Client.AuthorityTokens to Ris.Application.Common.AuthorityTokens
    - update affected file
    
    --HG--
    branch : Rigel

--- Ris/Client/Admin/ValidationLiveEditTool.cs
+++ Ris/Client/Admin/ValidationLiveEditTool.cs
@@ -37,7 +37,7 @@ namespace ClearCanvas.Ris.Client.Admin
     /// Tool that allows editing of validation rules on a live application component.
     /// </summary>
     [MenuAction("launch", "applicationcomponent-metacontextmenu/Edit Validation Rules", "Launch")]
-	[ActionPermission("launch", ClearCanvas.Ris.Client.AuthorityTokens.Desktop.UIValidationRules)]
+	[ActionPermission("launch", ClearCanvas.Ris.Application.Common.AuthorityTokens.Desktop.UIValidationRules)]
 	[ExtensionOf(typeof(ApplicationComponentMetaToolExtensionPoint), FeatureToken = FeatureTokens.RIS.Core)]
     public class ValidationLiveEditTool : Tool<IApplicationComponentMetaToolContext>
     {
