135cb31128eedd398a69b85b7201752c0637e059
Jasper Yeh <jasper@clearcanvas.ca>
2014-05-15 15:39:03 -0400

    [Requirement #12217] - Cleome RIS must be localization-ready, with a test Spanish translation * Moved some more localizable strings to resources.
    --HG--
    branch : Orion
    extra : rebase_source : 04d4d29a09e0dc462796be994d1ff07eaafdc280

--- Desktop/DesktopMonitorComponent.cs
+++ Desktop/DesktopMonitorComponent.cs
@@ -23,6 +23,7 @@
 #endregion
 
 #pragma warning disable 1591
+// ReSharper disable LocalizableElement
 
 using System;
 using ClearCanvas.Common;
@@ -551,3 +552,5 @@ namespace ClearCanvas.Desktop
         }
     }
 }
+
+// ReSharper restore LocalizableElement
--- Desktop/View/WinForms/ExceptionDialogControl.cs
+++ Desktop/View/WinForms/ExceptionDialogControl.cs
@@ -29,13 +29,16 @@ using ClearCanvas.Desktop.Actions;
 
 namespace ClearCanvas.Desktop.View.WinForms
 {
-    internal partial class ExceptionDialogControl : CustomUserControl
-    {
-    	private readonly Exception _exception;
+	internal partial class ExceptionDialogControl : CustomUserControl
+	{
+		private readonly Exception _exception;
 
 		internal ExceptionDialogControl(Exception exception, string message, ExceptionDialogActions buttonActions, ClickHandlerDelegate ok, ClickHandlerDelegate quit)
 		{
-        	_exception = exception;
+			const string errorOk = "Ok method must be supplied";
+			const string errorQuit = "Quit method must be supplied";
+
+			_exception = exception;
 
 			InitializeComponent();
 
@@ -59,31 +62,34 @@ namespace ClearCanvas.Desktop.View.WinForms
 			if (buttonActions == ExceptionDialogActions.Ok)
 			{
 				if (ok == null)
-					throw new ArgumentException("Ok method must be supplied", "ok");
+					throw new ArgumentException(errorOk, "ok");
 
 				_okButton.Click += okClick;
 				_quitButton.Dispose();
 			}
-			else if (buttonActions == ExceptionDialogActions.Quit)
-			{
-				if (quit == null)
-					throw new ArgumentException("Quit method must be supplied", "quit");
-
-				_quitButton.Click += quitClick;
-				AcceptButton = _quitButton;
-				CancelButton = _quitButton;
-				_okButton.Dispose();
-			}
 			else
 			{
-				if (ok == null)
-					throw new ArgumentException("Ok method must be supplied", "ok");
-				if (quit == null)
-					throw new ArgumentException("Quit method must be supplied", "quit");
+				if (buttonActions == ExceptionDialogActions.Quit)
+				{
+					if (quit == null)
+						throw new ArgumentException(errorQuit, "quit");
 
-				_okButton.Click += okClick;
-				_okButton.Text = SR.MenuContinue;
-				_quitButton.Click += quitClick;
+					_quitButton.Click += quitClick;
+					AcceptButton = _quitButton;
+					CancelButton = _quitButton;
+					_okButton.Dispose();
+				}
+				else
+				{
+					if (ok == null)
+						throw new ArgumentException(errorOk, "ok");
+					if (quit == null)
+						throw new ArgumentException(errorQuit, "quit");
+
+					_okButton.Click += okClick;
+					_okButton.Text = SR.MenuContinue;
+					_quitButton.Click += quitClick;
+				}
 			}
 
 			if (_exception != null)
@@ -99,101 +105,101 @@ namespace ClearCanvas.Desktop.View.WinForms
 				_detailButton.Dispose();
 			}
 
-        	// Hide the details when dialog first startup
-            HideDetails();
-        }
+			// Hide the details when dialog first startup
+			HideDetails();
+		}
 
 		public ExceptionDialogAction Result { get; private set; }
 
-        #region Event functions
+		#region Event functions
 
-        private void _detailButton_Click(object sender, EventArgs e)
-        {
-            if (_detailTree.Visible)
-                HideDetails();
-            else
-                ShowDetails();
-        }
+		private void _detailButton_Click(object sender, EventArgs e)
+		{
+			if (_detailTree.Visible)
+				HideDetails();
+			else
+				ShowDetails();
+		}
 
-        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Copy exception details to clipboard
+		private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// Copy exception details to clipboard
 			string clipboardMessage = SR.ExceptionHandlerMessagePrefix + _description.Text + Environment.NewLine + Environment.NewLine;
-            clipboardMessage += BuildMessageFromException(_exception);
-            Clipboard.SetText(clipboardMessage);
-        }
+			clipboardMessage += BuildMessageFromException(_exception);
+			Clipboard.SetText(clipboardMessage);
+		}
 
-        #endregion
+		#endregion
 
-        #region Helper functions
+		#region Helper functions
 
-        private void HideDetails()
-        {
-            _detailTree.Hide();
-        	_detailButton.Text = SR.MenuShowDetails;
+		private void HideDetails()
+		{
+			_detailTree.Hide();
+			_detailButton.Text = SR.MenuShowDetails;
 
-            // Shrink the user control
+			// Shrink the user control
 			Height -= _detailTree.Height;
-        }
+		}
 
-        private void ShowDetails()
-        {
-            _detailTree.Show();
+		private void ShowDetails()
+		{
+			_detailTree.Show();
 			_detailButton.Text = SR.MenuHideDetails;
 
-            // Expand the user control
+			// Expand the user control
 			Height += _detailTree.Height;
-        }
+		}
 
-        private void BuildTreeFromException(TreeNode thisNode, Exception e)
-        {
-            // Special case for the root node
-            if (thisNode == null)
-                thisNode = _detailTree.Nodes.Add(e.Message);
-            else
-                thisNode.Nodes.Add(e.Message);
+		private void BuildTreeFromException(TreeNode thisNode, Exception e)
+		{
+			// Special case for the root node
+			if (thisNode == null)
+				thisNode = _detailTree.Nodes.Add(e.Message);
+			else
+				thisNode.Nodes.Add(e.Message);
 
-            if (e.StackTrace != null)
-            {
-                // Add a new node for each level of StackTrace
-                string lineBreak = Environment.NewLine;
-                int prevIndex = 0;
-                int startIndex = e.StackTrace.IndexOf(lineBreak, prevIndex);
-                while (startIndex != -1)
-                {
-                    thisNode.Nodes.Add(e.StackTrace.Substring(prevIndex, startIndex - prevIndex));
-                    prevIndex = startIndex + lineBreak.Length;
-                    startIndex = e.StackTrace.IndexOf(lineBreak, prevIndex);
-                }
-                thisNode.Nodes.Add(e.StackTrace.Substring(prevIndex));
-            }
+			if (e.StackTrace != null)
+			{
+				// Add a new node for each level of StackTrace
+				string lineBreak = Environment.NewLine;
+				int prevIndex = 0;
+				int startIndex = e.StackTrace.IndexOf(lineBreak, prevIndex);
+				while (startIndex != -1)
+				{
+					thisNode.Nodes.Add(e.StackTrace.Substring(prevIndex, startIndex - prevIndex));
+					prevIndex = startIndex + lineBreak.Length;
+					startIndex = e.StackTrace.IndexOf(lineBreak, prevIndex);
+				}
+				thisNode.Nodes.Add(e.StackTrace.Substring(prevIndex));
+			}
 
-            // Recursively add inner exception to the tree
-            if (e.InnerException != null)
-            {
-                TreeNode childNode = thisNode.Nodes.Add("InnerException");
-                BuildTreeFromException(childNode, e.InnerException);
-            }
-        }
+			// Recursively add inner exception to the tree
+			if (e.InnerException != null)
+			{
+				TreeNode childNode = thisNode.Nodes.Add("InnerException");
+				BuildTreeFromException(childNode, e.InnerException);
+			}
+		}
 
-        private static string BuildMessageFromException(Exception e)
-        {
-        	var sb = new StringBuilder();
+		private static string BuildMessageFromException(Exception e)
+		{
+			var sb = new StringBuilder();
 
 			sb.AppendLine(!string.IsNullOrEmpty(e.Source) ? string.Format(SR.FormatExceptionDetails, e.Source, e.Message) : e.Message);
-        	sb.AppendLine(e.StackTrace);
+			sb.AppendLine(e.StackTrace);
 
-            // Recursively add inner exception to the message
-            if (e.InnerException != null)
-            {
-            	sb.AppendLine();
+			// Recursively add inner exception to the message
+			if (e.InnerException != null)
+			{
+				sb.AppendLine();
 				sb.AppendLine(SR.ExceptionHandlerInnerExceptionText);
-            	sb.AppendLine(BuildMessageFromException(e.InnerException));
-            }
+				sb.AppendLine(BuildMessageFromException(e.InnerException));
+			}
 
-            return sb.ToString();
-        }
+			return sb.ToString();
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
