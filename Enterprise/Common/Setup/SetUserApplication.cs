489a5f78070f513a82f99eb5096a3b746aa66dbf
Jay Liu <jay@clearcanvas.ca>
2014-06-03 16:44:42 -0400

    [Ticket #12353] - Fix component installers to import/upgrade settings and tokens
     * create a SetUserApplication that allows client to add/update users via commandline
    
    --HG--
    branch : Orion

---
+++ Enterprise/Common/Setup/SetUserApplication.cs
@@ -0,0 +1,61 @@
+﻿#region License
+
+// Copyright (c) 2013, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This file is part of the ClearCanvas RIS/PACS open source project.
+//
+// The ClearCanvas RIS/PACS open source project is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// The ClearCanvas RIS/PACS open source project is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the ClearCanvas RIS/PACS open source project.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using System.Net;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Utilities;
+
+namespace ClearCanvas.Enterprise.Common.Setup
+{
+	/// <summary>
+	/// Application root for creating/updating specific user in the enterprise server.
+	/// </summary>
+	[ExtensionOf(typeof (ApplicationRootExtensionPoint))]
+	public class SetUserApplication : IApplicationRoot
+	{
+		#region IApplicationRoot Members
+
+		public void RunApplication(string[] args)
+		{
+			var cmdLine = new SetUserCommandLine();
+			try
+			{
+				cmdLine.Parse(args);
+
+				using (new AuthenticationScope(cmdLine.UserName, "setup", Dns.GetHostName(), cmdLine.Password))
+				{
+					SetupHelper.ImportUsers(cmdLine.UserData);
+				}
+			}
+			catch (CommandLineException e)
+			{
+				Console.WriteLine(e.Message);
+				Platform.Log(LogLevel.Error, e, "Command line error.");
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Enterprise/Common/Setup/UserDefinition.cs
@@ -0,0 +1,63 @@
+﻿#region License
+
+// Copyright (c) 2013, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This file is part of the ClearCanvas RIS/PACS open source project.
+//
+// The ClearCanvas RIS/PACS open source project is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// The ClearCanvas RIS/PACS open source project is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the ClearCanvas RIS/PACS open source project.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using System.Runtime.Serialization;
+
+namespace ClearCanvas.Enterprise.Common.Setup
+{
+	/// <summary>
+	/// Helper class for providing user definitions to be imported at deployment time.
+	/// </summary>
+	[DataContract]
+	public class UserDefinition
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		// Required for JSML deserialization - do not remove
+		public UserDefinition()
+		{
+			AuthorityGroups = new string[0];
+		}
+
+		[DataMember]
+		public string UserName;
+
+		[DataMember]
+		public string DisplayName;
+
+		[DataMember]
+		public bool Enabled;
+
+		[DataMember]
+		public string EmailAddress;
+
+		/// <summary>
+		/// Gets the set the authority groups which this user belongs to.
+		/// </summary>
+		[DataMember]
+		public string[] AuthorityGroups { get; set; }
+	}
+}
