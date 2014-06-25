053ff985c10bd5229db8f44c7350ef13021ff32d
Steve Wranovsky <steve@clearcanvas.ca>
2014-06-25 13:48:47 -0500

    [Ticket #12380] - Optimize query-related code in the ImageServer and DICOM toolkit
    The cache in the DeviceManager needed to take into account the partition.  Also did alittle code cleanup.
    
    --HG--
    branch : Orion

--- ImageServer/Services/Dicom/DeviceManager.cs
+++ ImageServer/Services/Dicom/DeviceManager.cs
@@ -33,118 +33,119 @@ using ClearCanvas.ImageServer.Model.EntityBrokers;
 
 namespace ClearCanvas.ImageServer.Services.Dicom
 {
-    class DeviceManager
-    {
-	    private static ServerCache<string, Device> _deviceCache = new ServerCache<string, Device>(TimeSpan.FromSeconds(150), TimeSpan.FromSeconds(30)); 
+	internal class DeviceManager
+	{
+		private static readonly ServerCache<string, Device> DeviceCache =
+			new ServerCache<string, Device>(TimeSpan.FromSeconds(150), TimeSpan.FromSeconds(30));
 
-    	/// <summary>
-    	/// Lookup the device entity in the database corresponding to the remote AE of the association.
-    	/// </summary>
-    	/// <param name="partition">The partition to look up the devices</param>
-    	/// <param name="association">The association</param>
-    	/// <param name="isNew">Indicates whether the device returned is created by the call.</param>
-    	/// <returns>The device record corresponding to the called AE of the association</returns>
-		static public Device LookupDevice(ServerPartition partition, AssociationParameters association, out bool isNew)
-    	{
-    		isNew = false;
+		/// <summary>
+		/// Lookup the device entity in the database corresponding to the remote AE of the association.
+		/// </summary>
+		/// <param name="partition">The partition to look up the devices</param>
+		/// <param name="association">The association</param>
+		/// <param name="isNew">Indicates whether the device returned is created by the call.</param>
+		/// <returns>The device record corresponding to the called AE of the association</returns>
+		public static Device LookupDevice(ServerPartition partition, AssociationParameters association, out bool isNew)
+		{
+			isNew = false;
 
-    		Device device = null;
-			if (_deviceCache.TryGetValue(association.CallingAE, out device))
+			Device device;
+			if (DeviceCache.TryGetValue(association.CallingAE + partition.Key, out device))
 			{
 				return device;
 			}
 
-    		using (
-    			IUpdateContext updateContext =
-    				PersistentStoreRegistry.GetDefaultStore().OpenUpdateContext(UpdateContextSyncMode.Flush))
-    		{
-    			var deviceEntityBroker = updateContext.GetBroker<IDeviceEntityBroker>();
+			using (
+				IUpdateContext updateContext =
+					PersistentStoreRegistry.GetDefaultStore().OpenUpdateContext(UpdateContextSyncMode.Flush))
+			{
+				var deviceEntityBroker = updateContext.GetBroker<IDeviceEntityBroker>();
 
-    			// Setup the select parameters.
-    			var queryParameters = new DeviceSelectCriteria();
-    			queryParameters.AeTitle.EqualTo(association.CallingAE);
-    			queryParameters.ServerPartitionKey.EqualTo(partition.GetKey());
-                var devices = deviceEntityBroker.Find(queryParameters);
-                foreach (var d in devices)
-                {                    
-                    if (string.Compare(d.AeTitle,association.CallingAE,false,CultureInfo.InvariantCulture) == 0)
-                    {
-                        device = d;
-                        break;
-                    }
-                }
+				// Setup the select parameters.
+				var queryParameters = new DeviceSelectCriteria();
+				queryParameters.AeTitle.EqualTo(association.CallingAE);
+				queryParameters.ServerPartitionKey.EqualTo(partition.GetKey());
+				var devices = deviceEntityBroker.Find(queryParameters);
+				foreach (var d in devices)
+				{
+					if (string.Compare(d.AeTitle, association.CallingAE, false, CultureInfo.InvariantCulture) == 0)
+					{
+						device = d;
+						break;
+					}
+				}
 
-    			if (device == null)
-    			{
-    				if (!partition.AcceptAnyDevice)
-    				{
-    					return null;
-    				}
+				if (device == null)
+				{
+					if (!partition.AcceptAnyDevice)
+					{
+						return null;
+					}
 
-    				if (partition.AutoInsertDevice)
-    				{
-    					// Auto-insert a new entry in the table.
-    				    var updateColumns = new DeviceUpdateColumns
-    				                            {
-    				                                AeTitle = association.CallingAE,
-    				                                Enabled = true,
-    				                                Description = String.Format("AE: {0}", association.CallingAE),
-    				                                Dhcp = false,
-    				                                IpAddress = association.RemoteEndPoint.Address.ToString(),
-    				                                ServerPartitionKey = partition.GetKey(),
-    				                                Port = partition.DefaultRemotePort,
-    				                                AllowQuery = true,
-    				                                AllowRetrieve = true,
-    				                                AllowStorage = true,
-    				                                ThrottleMaxConnections = ImageServerCommonConfiguration.Device.MaxConnections,
-    				                                DeviceTypeEnum = DeviceTypeEnum.Workstation
-    				                            };
+					if (partition.AutoInsertDevice)
+					{
+						// Auto-insert a new entry in the table.
+						var updateColumns = new DeviceUpdateColumns
+							{
+								AeTitle = association.CallingAE,
+								Enabled = true,
+								Description = String.Format("AE: {0}", association.CallingAE),
+								Dhcp = false,
+								IpAddress = association.RemoteEndPoint.Address.ToString(),
+								ServerPartitionKey = partition.GetKey(),
+								Port = partition.DefaultRemotePort,
+								AllowQuery = true,
+								AllowRetrieve = true,
+								AllowStorage = true,
+								ThrottleMaxConnections = ImageServerCommonConfiguration.Device.MaxConnections,
+								DeviceTypeEnum = DeviceTypeEnum.Workstation
+							};
 
-    				    var insert = updateContext.GetBroker<IDeviceEntityBroker>();
+						var insert = updateContext.GetBroker<IDeviceEntityBroker>();
 
-    					device = insert.Insert(updateColumns);
+						device = insert.Insert(updateColumns);
 
-    					updateContext.Commit();
+						updateContext.Commit();
 
-    					isNew = true;
-    				}
-    			}
+						isNew = true;
+					}
+				}
 
-    			if (device != null)
-    			{
-    				// For DHCP devices, we always update the remote ip address, if its changed from what is in the DB.
-    				if (device.Dhcp && !association.RemoteEndPoint.Address.ToString().Equals(device.IpAddress))
-    				{
-    				    var updateColumns = new DeviceUpdateColumns
-    				                            {
-    				                                IpAddress = association.RemoteEndPoint.Address.ToString(),
-    				                                LastAccessedTime = Platform.Time
-    				                            };
-
-						if (!deviceEntityBroker.Update(device.GetKey(), updateColumns))
-    						Platform.Log(LogLevel.Error,
-    						             "Unable to update IP Address for DHCP device {0} on partition '{1}'",
-    						             device.AeTitle, partition.Description);
-    					else
-    						updateContext.Commit();
-    				}
-    				else if (!isNew)
-    				{
-    				    var updateColumns = new DeviceUpdateColumns {LastAccessedTime = Platform.Time};
+				if (device != null)
+				{
+					// For DHCP devices, we always update the remote ip address, if its changed from what is in the DB.
+					if (device.Dhcp && !association.RemoteEndPoint.Address.ToString().Equals(device.IpAddress))
+					{
+						var updateColumns = new DeviceUpdateColumns
+							{
+								IpAddress = association.RemoteEndPoint.Address.ToString(),
+								LastAccessedTime = Platform.Time
+							};
 
 						if (!deviceEntityBroker.Update(device.GetKey(), updateColumns))
-    						Platform.Log(LogLevel.Error,
-    						             "Unable to update LastAccessedTime device {0} on partition '{1}'",
-    						             device.AeTitle, partition.Description);
-    					else
-    						updateContext.Commit();
-    				}
+							Platform.Log(LogLevel.Error,
+							             "Unable to update IP Address for DHCP device {0} on partition '{1}'",
+							             device.AeTitle, partition.Description);
+						else
+							updateContext.Commit();
+					}
+					else if (!isNew)
+					{
+						var updateColumns = new DeviceUpdateColumns {LastAccessedTime = Platform.Time};
 
-    				_deviceCache.Add(device.AeTitle, device);
-    			}
-    		}
+						if (!deviceEntityBroker.Update(device.GetKey(), updateColumns))
+							Platform.Log(LogLevel.Error,
+							             "Unable to update LastAccessedTime device {0} on partition '{1}'",
+							             device.AeTitle, partition.Description);
+						else
+							updateContext.Commit();
+					}
 
-    		return device;
-    	}
-    }
+					DeviceCache.Add(device.AeTitle + partition.Key, device);
+				}
+			}
+
+			return device;
+		}
+	}
 }
