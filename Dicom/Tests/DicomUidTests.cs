3d4859be610c3f6f72d4fd5575717dc0002ee471
Jasper Yeh <jasper.yeh@nullstack.net>
2014-08-09 17:06:12 -0400

    DicomUid generator code no longer uses BigInteger
    * All it takes is a bit of byte math.
    * Removed System.Numerics reference.
    
    Fixes #22

---
+++ Dicom/Tests/DicomUidTests.cs
@@ -0,0 +1,44 @@
+﻿#region License
+
+// Copyright (c) 2013, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This file is part of the ClearCanvas RIS/PACS open source project.
+//
+// The ClearCanvas RIS/PACS open source project is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// The ClearCanvas RIS/PACS open source project is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the ClearCanvas RIS/PACS open source project.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#if UNIT_TESTS
+
+using System;
+using NUnit.Framework;
+
+namespace ClearCanvas.Dicom.Tests
+{
+	[TestFixture]
+	internal class DicomUidTests
+	{
+		[Test]
+		public void TestGuidConversion()
+		{
+			// this is the reference sample for 2.25 style UIDs given on David Clunie's website (http://www.dclunie.com/medical-image-faq/html/part2.html)
+			Assert.AreEqual("329800735698586629295641978511506172918", DicomUid.ConvertGuid(new Guid("f81d4fae-7dec-11d0-a765-00a0c91e6bf6")));
+		}
+	}
+}
+
+#endif
