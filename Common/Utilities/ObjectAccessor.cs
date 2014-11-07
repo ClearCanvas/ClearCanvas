6f39a0e10bb6c40036e2d862970d86ae8facb46f
Jonathan Resnick <jonathan@clearcanvas.ca>
2014-11-07 01:13:51 -0500

    [Requirement #12672] - Added a mechanism for providing fast dynamic access to object properties
     * Added a class called ObjectAccessor, which provides an alternative to straight-up reflection when you need to dynamically set or get properties on an object.  Usage is:
    
            var x = new Person();
            var objAccessor = ObjectAccessor.For(typeof(Person));
            var propAccessor = a.GetProperty("Name");
            propAccessor.SetValue(x, "Bob");
            propAccessor.GetValue(x); // -> "Bob"
    
     * All object accessors and property accessors are cached globally, so the cost of creating the necessary delegates is only incurred once per property.
    
     * According to my tests, this performs ~ 8x faster than using reflection.
    
    --HG--
    branch : Polaris

---
+++ Common/Utilities/ObjectAccessor.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace ClearCanvas.Common.Utilities
+{
+	/// <summary>
+	/// Provides high-performance dynamic access to object properties.
+	/// </summary>
+	/// <remarks>
+	/// Both static and instance members of this class are safe for concurrent use by multiple threads.
+	/// </remarks>
+	public class ObjectAccessor
+	{
+		private static readonly ConcurrentDictionary<Type, ObjectAccessor> _typeAccessors = new ConcurrentDictionary<Type, ObjectAccessor>();
+
+		/// <summary>
+		/// Gets a cached instance of <see cref="ObjectAccessor"/> for the specified type.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static ObjectAccessor For(Type type)
+		{
+			return _typeAccessors.GetOrAdd(type, t => new ObjectAccessor(t));
+		}
+
+
+		private readonly Type _type;
+		private readonly Dictionary<string, PropertyAccessor> _propertyAccessors = new Dictionary<string, PropertyAccessor>();
+
+		private ObjectAccessor(Type type)
+		{
+			_type = type;
+		}
+
+		/// <summary>
+		/// Gets the <see cref="PropertyAccessor"/> for the specified property, or null if the property cannot be found.
+		/// </summary>
+		/// <param name="name">The name of the property to get.</param>
+		/// <param name="includeNonPublic">True to include non-public properties in the search.</param>
+		/// <returns></returns>
+		[CanBeNull]
+		public PropertyAccessor GetProperty(string name, bool includeNonPublic = false)
+		{
+			lock (_propertyAccessors)
+			{
+				PropertyAccessor result;
+				if (_propertyAccessors.TryGetValue(name, out result))
+					return result;
+			}
+
+			// do the expensive stuff outside of the lock
+			var bindingFlags = BindingFlags.Instance | BindingFlags.Public;
+			if (includeNonPublic)
+				bindingFlags |= BindingFlags.NonPublic;
+
+			var propertyInfo = _type.GetProperty(name, bindingFlags);
+			if (propertyInfo == null)
+				return null;
+			
+			var accessor = PropertyAccessor.Create(_type, propertyInfo);
+			lock (_propertyAccessors)
+			{
+				// did another thread create the same accessor while we did?
+				// if so, return that one (so that we're always returning the same instance)
+				PropertyAccessor result;
+				if (_propertyAccessors.TryGetValue(name, out result))
+					return result;
+
+				_propertyAccessors.Add(name, accessor);
+				return accessor;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Provides high-performance dynamic access to a property.
+	/// </summary>
+	/// <remarks>
+	/// Both static and instance members of this class are safe for concurrent use by multiple threads.
+	/// </remarks>
+	public abstract class PropertyAccessor
+	{
+		internal static PropertyAccessor Create(Type type, PropertyInfo propertyInfo)
+		{
+			var genericType = typeof(PropertyAccessor<,>).MakeGenericType(type, propertyInfo.PropertyType);
+			return (PropertyAccessor)Activator.CreateInstance(genericType, BindingFlags.NonPublic|BindingFlags.Instance, null, new object[]{type, propertyInfo}, null);
+		}
+
+		/// <summary>
+		/// Gets the <see cref="PropertyInfo"/> for the property associated with this accessor.
+		/// </summary>
+		public abstract PropertyInfo Property { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether the property is readable (has a getter).
+		/// </summary>
+		public abstract bool IsReadable { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether the property is writable (has a setter).
+		/// </summary>
+		public abstract bool IsWritable { get; }
+
+		/// <summary>
+		/// Gets the value of the property for the specified object.
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public abstract object GetValue(object obj);
+
+		/// <summary>
+		/// Sets the value of the property on the the specified object.
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <param name="value"></param>
+		public abstract void SetValue(object obj, object value);
+	}
+
+	internal class PropertyAccessor<TObject, TProperty> : PropertyAccessor
+	{
+		private readonly Type _type;
+		private readonly PropertyInfo _property;
+		private readonly Func<TObject, TProperty> _getter;
+		private readonly Action<TObject, TProperty> _setter;
+
+		private PropertyAccessor(Type type, PropertyInfo propertyInfo)
+		{
+			_type = type;
+			_property = propertyInfo;
+
+			// init everything up front so that we're immutable and therefore thread safe
+			var getMethod = propertyInfo.GetGetMethod(true);
+			if (getMethod != null)
+			{
+				_getter = (Func<TObject, TProperty>)Delegate.CreateDelegate(typeof(Func<TObject, TProperty>), getMethod);
+			}
+
+			var setMethod = propertyInfo.GetSetMethod(true);
+			if (setMethod != null)
+			{
+				_setter = (Action<TObject, TProperty>)Delegate.CreateDelegate(typeof(Action<TObject, TProperty>), setMethod);
+			}
+		}
+
+		public override PropertyInfo Property
+		{
+			get { return _property; }
+		}
+
+		public override bool IsReadable
+		{
+			get { return _getter != null; }
+		}
+
+		public override bool IsWritable
+		{
+			get { return _setter != null; }
+		}
+
+		public override object GetValue(object obj)
+		{
+			if (_getter == null)
+				throw new InvalidOperationException(string.Format("Property {0}.{1} does not have a getter.", _type.Name, _property.Name));
+			return _getter((TObject)obj);
+		}
+
+		public override void SetValue(object obj, object value)
+		{
+			if (_setter == null)
+				throw new InvalidOperationException(string.Format("Property {0}.{1} does not have a setter.", _type.Name, _property.Name));
+			_setter((TObject)obj, (TProperty)value);
+		}
+	}
+}
---
+++ Common/Utilities/Tests/ObjectAccessorTests.cs
@@ -0,0 +1,253 @@
+﻿#region License
+
+// Copyright (c) 2013, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This file is part of the ClearCanvas RIS/PACS open source project.
+//
+// The ClearCanvas RIS/PACS open source project is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// The ClearCanvas RIS/PACS open source project is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the ClearCanvas RIS/PACS open source project.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using NUnit.Framework;
+
+#if UNIT_TESTS
+
+namespace ClearCanvas.Common.Utilities.Tests
+{
+	[TestFixture]
+	class ObjectAccessorTests
+	{
+		class Foo
+		{
+			public string _readOnlyPropBackingField;
+			public string _writeOnlyPropBackingField;
+
+			public Foo(
+				string readOnlyPropBackingField = null,
+				string writeOnlyPropBackingField = null,
+				string refProp = null,
+				int valueProp = 0,
+				string publicProp = null,
+				string semiPublicProp = null,
+				string privateProp = null
+			)
+			{
+				_readOnlyPropBackingField = readOnlyPropBackingField;
+				_writeOnlyPropBackingField = writeOnlyPropBackingField;
+				RefProp = refProp;
+				ValueProp = valueProp;
+				PublicProp = publicProp;
+				SemiPublicProp = semiPublicProp;
+				PrivateProp = privateProp;
+			}
+
+			public string RefProp { get; set; }
+			public int ValueProp { get; set; }
+
+			public string PublicProp { get; set; }
+			public string SemiPublicProp { get; private set; }
+			private string PrivateProp { get; set; }
+
+			public string ReadOnlyProp
+			{
+				get { return _readOnlyPropBackingField; }
+			}
+
+			public string WriteOnlyProp
+			{
+				set { _writeOnlyPropBackingField = value; }
+			}
+
+			public string GetPrivatePropValue()
+			{
+				return PrivateProp;
+			}
+		}
+
+
+		[Test]
+		public void Test_get_nonexistent_property_returns_null()
+		{
+			var propAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("NonExistantProp");
+			Assert.IsNull(propAccessor);
+		}
+
+		[Test]
+		public void Test_reference_property()
+		{
+			var foo = new Foo(refProp: "red");
+			var propAccessor = ObjectAccessor.For(typeof (Foo)).GetProperty("RefProp");
+			Assert.IsNotNull(propAccessor);
+			Assert.IsTrue(propAccessor.IsReadable);
+			Assert.IsTrue(propAccessor.IsWritable);
+
+			var value = propAccessor.GetValue(foo);
+			Assert.AreEqual("red", value);
+
+			propAccessor.SetValue(foo, "blue");
+			Assert.AreEqual("blue", foo.RefProp);
+		}
+
+		[Test]
+		public void Test_value_property()
+		{
+			var foo = new Foo(valueProp: 12);
+			var propAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("ValueProp");
+			Assert.IsNotNull(propAccessor);
+			Assert.IsTrue(propAccessor.IsReadable);
+			Assert.IsTrue(propAccessor.IsWritable);
+
+			var value = propAccessor.GetValue(foo);
+			Assert.AreEqual(12, value);
+
+			propAccessor.SetValue(foo, 13);
+			Assert.AreEqual(13, foo.ValueProp);
+		}
+
+		[Test]
+		public void Test_public_property()
+		{
+			var foo = new Foo(publicProp: "red");
+			var publicAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("PublicProp", false);
+			Assert.IsNotNull(publicAccessor);
+			Assert.IsTrue(publicAccessor.IsReadable);
+			Assert.IsTrue(publicAccessor.IsWritable);
+
+			var value = publicAccessor.GetValue(foo);
+			Assert.AreEqual("red", value);
+
+			publicAccessor.SetValue(foo, "blue");
+			Assert.AreEqual("blue", foo.PublicProp);
+
+			var nonPublicAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("PublicProp", true);
+			Assert.IsNotNull(nonPublicAccessor);
+			Assert.IsTrue(nonPublicAccessor.IsReadable);
+			Assert.IsTrue(nonPublicAccessor.IsWritable);
+
+			value = nonPublicAccessor.GetValue(foo);
+			Assert.AreEqual("blue", value);
+
+			nonPublicAccessor.SetValue(foo, "red");
+			Assert.AreEqual("red", foo.PublicProp);
+		}
+
+		[Test]
+		public void Test_semipublic_property()
+		{
+			var foo = new Foo(semiPublicProp: "red");
+			var publicAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("SemiPublicProp", false);
+			Assert.IsNotNull(publicAccessor);
+			Assert.IsTrue(publicAccessor.IsReadable);
+
+			// note that the semi-public property is writable
+			// we don't respect the private setter, since the PropertyAccessor as a whole
+			// can really only be one or the other
+			Assert.IsTrue(publicAccessor.IsWritable);
+
+			var value = publicAccessor.GetValue(foo);
+			Assert.AreEqual("red", value);
+
+			publicAccessor.SetValue(foo, "blue");
+			Assert.AreEqual("blue", foo.SemiPublicProp);
+
+			var nonPublicAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("SemiPublicProp", true);
+			Assert.IsNotNull(nonPublicAccessor);
+			Assert.IsTrue(nonPublicAccessor.IsReadable);
+			Assert.IsTrue(nonPublicAccessor.IsWritable);
+
+			value = nonPublicAccessor.GetValue(foo);
+			Assert.AreEqual("blue", value);
+
+			nonPublicAccessor.SetValue(foo, "red");
+			Assert.AreEqual("red", foo.SemiPublicProp);
+		}
+
+		[Test]
+		public void Test_private_property()
+		{
+			var foo = new Foo(privateProp: "red");
+			var publicAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("PrivateProp", false);
+			Assert.IsNull(publicAccessor); // can't get public accessor to private property
+
+			var nonPublicAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("PrivateProp", true);
+			Assert.IsNotNull(nonPublicAccessor);
+			Assert.IsTrue(nonPublicAccessor.IsReadable);
+			Assert.IsTrue(nonPublicAccessor.IsWritable);
+
+			var value = nonPublicAccessor.GetValue(foo);
+			Assert.AreEqual("red", value);
+
+			nonPublicAccessor.SetValue(foo, "blue");
+			Assert.AreEqual("blue", foo.GetPrivatePropValue());
+		}
+
+		[Test]
+		public void Test_readonly_property()
+		{
+			var foo = new Foo(readOnlyPropBackingField: "red");
+			var propAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("ReadOnlyProp");
+			Assert.IsNotNull(propAccessor);
+			Assert.IsTrue(propAccessor.IsReadable);
+			Assert.IsFalse(propAccessor.IsWritable);
+
+			var value = propAccessor.GetValue(foo);
+			Assert.AreEqual("red", value);
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void Test_readonly_property_throws_on_write_attempt()
+		{
+			var foo = new Foo(readOnlyPropBackingField: "red");
+			var propAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("ReadOnlyProp");
+			Assert.IsNotNull(propAccessor);
+			Assert.IsTrue(propAccessor.IsReadable);
+			Assert.IsFalse(propAccessor.IsWritable);
+
+			propAccessor.SetValue(foo, "blue");
+		}
+
+		[Test]
+		public void Test_writeonly_property()
+		{
+			var foo = new Foo(writeOnlyPropBackingField: "red");
+			var propAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("WriteOnlyProp");
+			Assert.IsNotNull(propAccessor);
+			Assert.IsFalse(propAccessor.IsReadable);
+			Assert.IsTrue(propAccessor.IsWritable);
+
+			propAccessor.SetValue(foo, "blue");
+			Assert.AreEqual("blue", foo._writeOnlyPropBackingField);
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void Test_writeonly_property_throws_on_read_attempt()
+		{
+			var foo = new Foo(writeOnlyPropBackingField: "red");
+			var propAccessor = ObjectAccessor.For(typeof(Foo)).GetProperty("WriteOnlyProp");
+			Assert.IsNotNull(propAccessor);
+			Assert.IsFalse(propAccessor.IsReadable);
+			Assert.IsTrue(propAccessor.IsWritable);
+
+			propAccessor.GetValue(foo);
+		}
+	}
+}
+
+#endif
